a0c77ece82e70c7a5aa3ffb7353bb3e7f66e215e
Josef Söntgen <jsoentgen@gmail.com>
2012-07-12 13:39:31 +0200

    libports: swap nsap_addr.c in libc-inet/resolv

--- libports/lib/mk/libc-inet.mk
+++ libports/lib/mk/libc-inet.mk
@@ -1,5 +1,7 @@
 LIBC_INET_DIR = $(LIBC_DIR)/libc/inet
 
+FILTER_OUT_C += nsap_addr.c
+
 SRC_C = $(filter-out $(FILTER_OUT_C),$(notdir $(wildcard $(LIBC_INET_DIR)/*.c)))
 
 include $(REP_DIR)/lib/mk/libc-common.inc
--- libports/lib/mk/libc-resolv.mk
+++ libports/lib/mk/libc-resolv.mk
@@ -2,6 +2,9 @@ LIBC_RESOLV_DIR = $(LIBC_DIR)/libc/resolv
 
 SRC_C = $(notdir $(wildcard $(LIBC_RESOLV_DIR)/*.c))
 
+SRC_C += nsap_addr.c
+
 include $(REP_DIR)/lib/mk/libc-common.inc
 
 vpath %.c $(LIBC_RESOLV_DIR)
+vpath nsap_addr.c $(LIBC_DIR)/libc/inet
