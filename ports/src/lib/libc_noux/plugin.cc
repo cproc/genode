f8980bcb0999012db2f6441b895905b933c486c8
Christian Prochaska <christian.prochaska@genode-labs.com>
2012-08-28 17:37:59 +0200

    issue322.diff

--- ports/src/lib/libc_noux/plugin.cc
+++ ports/src/lib/libc_noux/plugin.cc
@@ -176,19 +176,6 @@ extern "C" uid_t geteuid()
 }
 
 
-extern "C" int __getcwd(char *dst, Genode::size_t dst_size)
-{
-	noux()->syscall(Noux::Session::SYSCALL_GETCWD);
-	Genode::size_t path_size = Genode::strlen(sysio()->getcwd_out.path);
-
-	if (dst_size < path_size + 1)
-		return -ERANGE;
-
-	Genode::strncpy(dst, sysio()->getcwd_out.path, dst_size);
-	return 0;
-}
-
-
 /**
  * Utility to copy-out syscall results to buf struct
  *
@@ -230,9 +217,6 @@ static int _stat(char const *path, struct stat *buf, bool lstat = false)
 }
 
 
-extern "C" int lstat(char const *path, struct stat *buf) { return _stat(path, buf, true);  }
-
-
 static bool serialize_string_array(char const * const * array, char *dst, Genode::size_t dst_len)
 {
 	for (unsigned i = 0; array[i]; i++)
@@ -248,61 +232,6 @@ static bool serialize_string_array(char const * const * array, char *dst, Genode
 
 	dst[0] = 0;
 	return true;
-}
-
-
-extern "C" int execve(char const *filename, char *const argv[],
-                      char *const envp[])
-{
-	if (verbose) {
-		PDBG("filename=%s", filename);
-
-		for (int i = 0; argv[i]; i++)
-			PDBG("argv[%d]='%s'", i, argv[i]);
-
-		for (int i = 0; envp[i]; i++)
-			PDBG("envp[%d]='%s'", i, envp[i]);
-	}
-
-	Genode::strncpy(sysio()->execve_in.filename, filename, sizeof(sysio()->execve_in.filename));
-	if (!serialize_string_array(argv, sysio()->execve_in.args,
-	                           sizeof(sysio()->execve_in.args))) {
-	    PERR("execve: argument buffer exceeded");
-	    errno = E2BIG;
-	    return -1;
-	}
-
-	if (!serialize_string_array(envp, sysio()->execve_in.env,
-	                            sizeof(sysio()->execve_in.env))) {
-	    PERR("execve: environment buffer exceeded");
-	    errno = E2BIG;
-	    return -1;
-	}
-
-	if (!noux()->syscall(Noux::Session::SYSCALL_EXECVE)) {
-		PWRN("exec syscall failed for path \"%s\"", filename);
-		switch (sysio()->error.execve) {
-		case Noux::Sysio::EXECVE_NONEXISTENT: errno = ENOENT; break;
-		}
-		return -1;
-	}
-
-	/*
-	 * In the success case, we never return from execve, the execution is
-	 * resumed in the new program.
-	 */
-	Genode::sleep_forever();
-	return 0;
-}
-
-
-/**
- * Called by execvp
- */
-extern "C" int _execve(char const *filename, char *const argv[],
-                       char *const envp[])
-{
-	return execve(filename, argv, envp);
 }
 
 
@@ -635,20 +564,25 @@ namespace {
 				_stderr(Libc::file_descriptor_allocator()->alloc(this, noux_context(2), 2))
 			{ }
 
-			bool supports_chdir(char const *)                { return true; }
-			bool supports_open(char const *, int)            { return true; }
-			bool supports_stat(char const *)                 { return true; }
-			bool supports_pipe()                             { return true; }
-			bool supports_unlink(char const *)               { return true; }
-			bool supports_rename(const char *, const char *) { return true; }
-			bool supports_mkdir(const char *, mode_t)        { return true; }
-			bool supports_socket(int, int, int)              { return true; }
-			bool supports_mmap()                             { return true; }
+			bool supports_execve(char const *, char *const[],
+			                     char *const[])                  { return true; }
+			bool supports_open(char const *, int)                { return true; }
+			bool supports_stat(char const *)                     { return true; }
+			bool supports_symlink(char const *, char const*)     { return true; }
+			bool supports_pipe()                                 { return true; }
+			bool supports_unlink(char const *)                   { return true; }
+			bool supports_readlink(const char *, char *, size_t) { return true; }
+			bool supports_rename(const char *, const char *)     { return true; }
+			bool supports_mkdir(const char *, mode_t)            { return true; }
+			bool supports_socket(int, int, int)                  { return true; }
+			bool supports_mmap()                                 { return true; }
 
 			Libc::File_descriptor *open(char const *, int);
 			ssize_t write(Libc::File_descriptor *, const void *, ::size_t);
 			int close(Libc::File_descriptor *);
 			int dup2(Libc::File_descriptor *, Libc::File_descriptor *);
+			int execve(char const *filename, char *const argv[],
+			           char *const envp[]);
 			int fstat(Libc::File_descriptor *, struct stat *);
 			int fsync(Libc::File_descriptor *);
 			int fstatfs(Libc::File_descriptor *, struct statfs *);
@@ -656,9 +590,10 @@ namespace {
 			int fcntl(Libc::File_descriptor *, int, long);
 			ssize_t getdirentries(Libc::File_descriptor *, char *, ::size_t, ::off_t *);
 			::off_t lseek(Libc::File_descriptor *, ::off_t offset, int whence);
-			int fchdir(Libc::File_descriptor *);
 			ssize_t read(Libc::File_descriptor *, void *, ::size_t);
+			ssize_t readlink(const char *path, char *buf, size_t bufsiz);
 			int stat(char const *, struct stat *);
+			int symlink(const char *, const char *);
 			int ioctl(Libc::File_descriptor *, int request, char *argp);
 			int pipe(Libc::File_descriptor *pipefd[2]);
 			int unlink(char const *path);
@@ -692,6 +627,51 @@ namespace {
 				       socklen_t);
 			int shutdown(Libc::File_descriptor *, int how);
 	};
+
+
+	int Plugin::execve(char const *filename, char *const argv[],
+	                   char *const envp[])
+	{
+		if (verbose) {
+			PDBG("filename=%s", filename);
+
+			for (int i = 0; argv[i]; i++)
+				PDBG("argv[%d]='%s'", i, argv[i]);
+
+			for (int i = 0; envp[i]; i++)
+				PDBG("envp[%d]='%s'", i, envp[i]);
+		}
+
+		Genode::strncpy(sysio()->execve_in.filename, filename, sizeof(sysio()->execve_in.filename));
+		if (!serialize_string_array(argv, sysio()->execve_in.args,
+		                           sizeof(sysio()->execve_in.args))) {
+		    PERR("execve: argument buffer exceeded");
+		    errno = E2BIG;
+		    return -1;
+		}
+
+		if (!serialize_string_array(envp, sysio()->execve_in.env,
+		                            sizeof(sysio()->execve_in.env))) {
+		    PERR("execve: environment buffer exceeded");
+		    errno = E2BIG;
+		    return -1;
+		}
+
+		if (!noux()->syscall(Noux::Session::SYSCALL_EXECVE)) {
+			PWRN("exec syscall failed for path \"%s\"", filename);
+			switch (sysio()->error.execve) {
+			case Noux::Sysio::EXECVE_NONEXISTENT: errno = ENOENT; break;
+			}
+			return -1;
+		}
+
+		/*
+		 * In the success case, we never return from execve, the execution is
+		 * resumed in the new program.
+		 */
+		Genode::sleep_forever();
+		return 0;
+	}
 
 
 	int Plugin::stat(char const *path, struct stat *buf)
@@ -744,9 +724,34 @@ namespace {
 		Libc::Plugin_context *context = noux_context(sysio()->open_out.fd);
 		Libc::File_descriptor *fd =
 		    Libc::file_descriptor_allocator()->alloc(this, context, sysio()->open_out.fd);
+		fd->path(pathname);
 		if ((flags & O_TRUNC) && (ftruncate(fd, 0) == -1))
 			return 0;
 		return fd;
+	}
+
+
+	int Plugin::symlink(const char *oldpath, const char *newpath)
+	{
+		if (verbose)
+			PDBG("%s -> %s", newpath, oldpath);
+
+		if ((Genode::strlen(oldpath) + 1 > sizeof(sysio()->symlink_in.oldpath)) ||
+		    (Genode::strlen(newpath) + 1 > sizeof(sysio()->symlink_in.newpath))) {
+			PDBG("ENAMETOOLONG");
+			errno = ENAMETOOLONG;
+			return 0;
+		}
+
+		Genode::strncpy(sysio()->symlink_in.oldpath, oldpath, sizeof(sysio()->symlink_in.oldpath));
+		Genode::strncpy(sysio()->symlink_in.newpath, newpath, sizeof(sysio()->symlink_in.newpath));
+		if (!noux()->syscall(Noux::Session::SYSCALL_SYMLINK)) {
+			PERR("symlink error");
+			/* XXX set errno */
+			return -1;
+		}
+
+		return 0;
 	}
 
 
@@ -1001,8 +1006,7 @@ namespace {
 				 */
 				Libc::File_descriptor *new_fd =
 					Libc::file_descriptor_allocator()->alloc(this, 0);
-
-				new_fd->context = noux_context(new_fd->libc_fd);
+				new_fd->path(fd->fd_path);
 
 				/*
 				 * Use new allocated number as name of file descriptor
@@ -1137,21 +1141,6 @@ namespace {
 	}
 
 
-	int Plugin::fchdir(Libc::File_descriptor *fd)
-	{
-		sysio()->fchdir_in.fd = noux_fd(fd->context);
-		if (!noux()->syscall(Noux::Session::SYSCALL_FCHDIR)) {
-			switch (sysio()->error.fchdir) {
-				case Noux::Sysio::FCHDIR_ERR_NOT_DIR: errno = ENOTDIR; break;
-				default:                              errno = EPERM;  break;
-			}
-			return -1;
-		}
-
-		return 0;
-	}
-
-
 	int Plugin::unlink(char const *path)
 	{
 		Genode::strncpy(sysio()->unlink_in.path, path, sizeof(sysio()->unlink_in.path));
@@ -1166,6 +1155,31 @@ namespace {
 		}
 
 		return 0;
+	}
+
+
+	ssize_t Plugin::readlink(const char *path, char *buf, size_t bufsiz)
+	{
+		if (verbose)
+			PDBG("path = %s, bufsiz = %zu", path, bufsiz);
+
+		Genode::strncpy(sysio()->readlink_in.path, path, sizeof(sysio()->readlink_in.path));
+		sysio()->readlink_in.bufsiz = bufsiz;
+
+		if (!noux()->syscall(Noux::Session::SYSCALL_READLINK)) {
+			PWRN("readlink syscall failed for \"%s\"", path);
+			/* XXX set errno */
+			return -1;
+		}
+
+		ssize_t size = Genode::min((size_t)sysio()->readlink_out.count, bufsiz);
+
+		Genode::memcpy(buf, sysio()->readlink_out.chunk, size);
+
+		if (verbose)
+			PDBG("result = %s", buf);
+
+		return size;
 	}
 
 
