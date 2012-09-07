27d3ec86292a59f8a367cda5f4049a84a040bb88
Taru Karttunen <taruti@taruti.net>
2012-09-07 23:00:51 +0300

    gcc 4.7 - small fix for base-hw

--- base-hw/src/core/kernel.cc
+++ base-hw/src/core/kernel.cc
@@ -256,7 +256,7 @@ namespace Kernel
 					Entry * find(unsigned long const object_id)
 					{
 						if (object_id == id()) return this;
-						Entry * const subtree = child(object_id > id());
+						Entry * const subtree = this->child(object_id > id());
 						return subtree ? subtree->find(object_id) : 0;
 					}
 
