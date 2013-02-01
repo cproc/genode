ea5e917080eddd312991fa537438ead8632e7729
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-02-01 18:01:24 +0100

    linux_lock_debug.diff

--- base-linux/src/base/lock/lock_helper.h
+++ base-linux/src/base/lock/lock_helper.h
@@ -19,6 +19,7 @@
 
 /* Genode includes */
 #include <base/native_types.h>
+#include <base/snprintf.h>
 #include <base/thread.h>
 
 /* Linux includes */
@@ -44,7 +45,19 @@ static inline void thread_yield()
 
 static inline bool thread_check_stopped_and_restart(Genode::Native_thread_id tid)
 {
+//#if 0
+	char name[32] = { 0 };
+	if (Genode::Thread_base::myself())
+		Genode::Thread_base::myself()->name(name, sizeof (name) - 1);
+	char buf[128];
+	Genode::snprintf(buf, sizeof(buf), "%d (%s): sending signal to thread %d\n", lx_gettid(), name, tid.tid);
+	raw_write_str(buf);
+//#endif
 	lx_tgkill(tid.pid, tid.tid, LX_SIGUSR1);
+//#if 0
+	Genode::snprintf(buf, sizeof(buf), "%d (%s): sent signal to thread %d\n", lx_gettid(), name, tid.tid);
+	raw_write_str(buf);
+//#endif
 	return true;
 }
 
@@ -75,6 +88,24 @@ static inline void thread_switch_to(Genode::Native_thread_id tid)
 
 static inline void thread_stop_myself()
 {
+//#if 0
+	char name[32] = { 0 };
+	if (Genode::Thread_base::myself())
+		Genode::Thread_base::myself()->name(name, sizeof (name) - 1);
+	char buf[128];
+	Genode::snprintf(buf, sizeof(buf), "%d (%s): thread_stop_myself() called\n", lx_gettid(), name);
+	raw_write_str(buf);
+//#endif
 	struct timespec ts = { 1000, 0 };
-	while (lx_nanosleep(&ts, 0) == 0);
+	//while (lx_nanosleep(&ts, 0) == 0);
+	int ret = 0;
+	while (ret == 0) {
+		ret = lx_nanosleep(&ts, 0);
+		Genode::snprintf(buf, sizeof(buf), "%d (%s): lx_nanosleep() returned %d\n", lx_gettid(), name, ret);
+		raw_write_str(buf);
+	}
+//#if 0
+	Genode::snprintf(buf, sizeof(buf), "%d (%s): thread_stop_myself() returns\n", lx_gettid(), name);
+	raw_write_str(buf);
+//#endif
 }
