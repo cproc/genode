791f9fca9c659aaa09846260eabd67de621bc929
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-02-07 21:41:00 +0100

    lock_test.diff

---
+++ base/src/test/lock/main.cc
@@ -0,0 +1,43 @@
+/* Genode includes */
+#include <base/thread.h>
+#include <base/printf.h>
+
+enum { STACK_SIZE = 1024 * sizeof(Genode::addr_t) };
+
+using namespace Genode;
+
+static Lock thread_lock;
+static Lock main_lock;
+
+struct Test_thread : Genode::Thread<STACK_SIZE>
+{
+	void entry()
+	{
+		unsigned long long count = 0;
+
+		for (;;) {
+			thread_lock.lock();
+			main_lock.unlock();
+			if ((++count % 100000) == 0)
+				PDBG("%llu: still running", count);
+		}
+	}
+};
+
+
+int main(int, char **)
+{
+	static Test_thread thread;
+	thread.start();
+
+	unsigned long long count = 0;
+
+	for (;;) {
+		main_lock.lock();
+		thread_lock.unlock();
+		if ((++count % 100000) == 0)
+			PDBG("%llu: still running", count);
+	}
+
+	return 0;
+}
