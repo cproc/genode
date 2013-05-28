cfc216aa4e7645404c07ece774f4e27b1f05c1c6
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-05-28 12:00:59 +0200

    issue345.diff

---
+++ qt5/src/lib/qt5/qtbase/src/plugins/platforms/nitpicker/qnitpickerwindowsurface.h
@@ -0,0 +1,46 @@
+/*
+ * \brief  QNitpickerWindowSurface
+ * \author Christian Prochaska
+ * \date   2013-05-08
+ */
+
+/*
+ * Copyright (C) 2013 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+
+#ifndef _QNITPICKERWINDOWSURFACE_H_
+#define _QNITPICKERWINDOWSURFACE_H_
+
+#include <qpa/qplatformbackingstore.h>
+
+class QNitpickerPlatformWindow;
+
+QT_BEGIN_NAMESPACE
+
+class QNitpickerWindowSurface : public QPlatformBackingStore
+{
+	private:
+
+		QNitpickerPlatformWindow *_platform_window;
+		QImage                    _image;
+		bool                      _framebuffer_changed;
+
+	public:
+
+		QNitpickerWindowSurface(QWindow *window);
+		~QNitpickerWindowSurface();
+
+		QPaintDevice *paintDevice();
+		void flush(QWindow *window, const QRegion &region, const QPoint &offset);
+		void resize(const QSize &size, const QRegion &staticContents);
+
+		void framebuffer_changed();
+};
+
+QT_END_NAMESPACE
+
+#endif /* _QNITPICKERWINDOWSURFACE_H_ */
