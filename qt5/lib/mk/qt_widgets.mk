c655f2c49164abc3060afd528b9fa0075bda317c
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-06-17 21:23:43 +0200

    issue345.diff

---
+++ qt5/lib/mk/qt_widgets.mk
@@ -0,0 +1,22 @@
+include $(REP_DIR)/lib/import/import-qt_widgets.mk
+
+SHARED_LIB = yes
+
+include $(REP_DIR)/lib/mk/qt_widgets_generated.inc
+
+# UI headers
+qfiledialog.o: ui_qfiledialog.h
+
+include $(REP_DIR)/lib/mk/qt.inc
+
+INC_DIR += $(REP_DIR)/include/qt5/qtbase/QtWidgets/private \
+           $(REP_DIR)/contrib/$(QT4)/qtbase/include/QtWidgets/$(QT_VERSION)/QtWidgets \
+           $(REP_DIR)/contrib/$(QT4)/qtbase/include/QtGui/$(QT_VERSION) \
+           $(REP_DIR)/contrib/$(QT4)/qtbase/include/QtGui/$(QT_VERSION)/QtGui \
+           $(REP_DIR)/contrib/$(QT4)/qtbase/include/QtCore/$(QT_VERSION) \
+           $(REP_DIR)/contrib/$(QT4)/qtbase/include/QtCore/$(QT_VERSION)/QtCore
+
+LIBS += qt_core libc
+
+#vpath % $(REP_DIR)/include/qt5/qtbase/QtWidgets
+#vpath % $(REP_DIR)/include/qt5/qtbase/QtWidgets/private
