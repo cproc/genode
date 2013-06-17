887880b4a545e9ffc88888451a6612c867e6010b
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-06-17 21:23:43 +0200

    issue345.diff

---
+++ qt5/src/lib/qt5/qtwebkit/Source/JavaScriptCore/generated/BooleanPrototype.lut.h
@@ -0,0 +1,15 @@
+// Automatically generated from /home/christian/projects/genode/genode/qt5/contrib/qt-everywhere-opensource-src-5.0.2/qtwebkit/Source/JavaScriptCore/runtime/BooleanPrototype.cpp using /home/christian/projects/genode/genode/qt5/contrib/qt-everywhere-opensource-src-5.0.2/qtwebkit/Source/JavaScriptCore/create_hash_table. DO NOT EDIT!
+
+#include "Lookup.h"
+
+namespace JSC {
+
+static const struct HashTableValue booleanPrototypeTableValues[3] = {
+   { "toString", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(booleanProtoFuncToString), (intptr_t)0, NoIntrinsic },
+   { "valueOf", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(booleanProtoFuncValueOf), (intptr_t)0, NoIntrinsic },
+   { 0, 0, 0, 0, NoIntrinsic }
+};
+
+extern const struct HashTable booleanPrototypeTable =
+    { 4, 3, booleanPrototypeTableValues, 0 };
+} // namespace
