93d53d2e25dcf2199ffe99d833746cf1dfec9426
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-07-24 18:41:29 +0200

    issue345.diff

---
+++ qt5/src/lib/qt5/qtwebkit/Source/JavaScriptCore/generated/JSONObject.lut.h
@@ -0,0 +1,15 @@
+// Automatically generated from /home/christian/projects/genode/genode/qt5/contrib/qt-everywhere-opensource-src-5.1.0/qtwebkit/Source/JavaScriptCore/runtime/JSONObject.cpp using /home/christian/projects/genode/genode/qt5/contrib/qt-everywhere-opensource-src-5.1.0/qtwebkit/Source/JavaScriptCore/create_hash_table. DO NOT EDIT!
+
+#include "Lookup.h"
+
+namespace JSC {
+
+static const struct HashTableValue jsonTableValues[3] = {
+   { "parse", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(JSONProtoFuncParse), (intptr_t)2, NoIntrinsic },
+   { "stringify", DontEnum|Function, (intptr_t)static_cast<NativeFunction>(JSONProtoFuncStringify), (intptr_t)3, NoIntrinsic },
+   { 0, 0, 0, 0, NoIntrinsic }
+};
+
+extern const struct HashTable jsonTable =
+    { 4, 3, jsonTableValues, 0 };
+} // namespace
