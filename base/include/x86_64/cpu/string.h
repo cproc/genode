cf4955c6e962cdf8b4d87d23dc5309523532f1d7
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-08-21 19:32:59 +0200

    memcpy_x86_64_rep_movsb.diff

--- base/include/x86_64/cpu/string.h
+++ base/include/x86_64/cpu/string.h
@@ -27,36 +27,15 @@ namespace Genode
 	 */
 	inline size_t memcpy_cpu(void *dst, const void *src, size_t size)
 	{
-		/* try to copy 32 byte chunks */
-		size_t chunk = size / 32;
-	
-		if (!chunk)
-			return size;
-	
 		asm volatile (
-			"emms                             \n\t"
-			"xor    %%rcx,%%rcx               \n\t"
-			".align 16                        \n\t"
-			"0:                               \n\t"
-			"movq   (%%rsi,%%rcx,8),%%mm0     \n\t"
-			"movq   8(%%rsi,%%rcx,8),%%mm1    \n\t"
-			"movq   16(%%rsi,%%rcx,8),%%mm2   \n\t"
-			"movq   24(%%rsi,%%rcx,8),%%mm3   \n\t"
-			"movntq %%mm0,(%%rdi,%%rcx,8)     \n\t"
-			"movntq %%mm1,8(%%rdi,%%rcx,8)    \n\t"
-			"movntq %%mm2,16(%%rdi,%%rcx,8)   \n\t"
-			"movntq %%mm3,24(%%rdi,%%rcx,8)   \n\t"
-			"add    $0x4, %%rcx               \n\t"
-			"dec    %2                        \n\t"
-			"jnz    0b                        \n\t"
-			"sfence                           \n\t"
-			"emms                             \n\t"
+			"cld \n\t"
+			"rep movsb \n\t"
 			:
-			: "S" (src), "D" (dst), "r" (chunk)
-			: "rcx", "memory"
+			: "S" (src), "D" (dst), "c" (size)
+			: "memory"
 		);
 	
-		return size % 32;
+		return 0;
 	}
 }
 
