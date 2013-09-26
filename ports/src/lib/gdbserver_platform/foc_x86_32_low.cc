6183f869c0020771f3d461a8106acfcaa7aefc25
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-09-26 19:11:38 +0200

    gdb_monitor.diff

--- ports/src/lib/gdbserver_platform/foc_x86_32_low.cc
+++ ports/src/lib/gdbserver_platform/foc_x86_32_low.cc
@@ -23,8 +23,8 @@ extern "C" {
 #include "gdbserver_platform_helper.h"
 #include "gdb_stub_thread.h"
 
-#undef PDBG
-#define PDBG(...)
+//#undef PDBG
+//#define PDBG(...)
 
 using namespace Genode;
 
@@ -109,3 +109,35 @@ extern "C" int genode_fetch_register(int regno, unsigned long *reg_content)
 
 	return -1;
 }
+
+extern "C" void genode_store_register(int regno, unsigned long reg_content)
+{
+	Thread_state thread_state;
+
+	try { thread_state = get_current_thread_state(); }
+	catch (...) { return; }
+
+	switch((enum reg_index)regno)
+	{
+		case EAX:  thread_state.eax    = reg_content; PDBG("EAX = %8lx", reg_content); break;
+		case ECX:  thread_state.ecx    = reg_content; PDBG("ECX = %8lx", reg_content); break;
+		case EDX:  thread_state.edx    = reg_content; PDBG("EDX = %8lx", reg_content); break;
+		case EBX:  thread_state.ebx    = reg_content; PDBG("EBX = %8lx", reg_content); break;
+		case UESP: thread_state.sp     = reg_content; PDBG("ESP = %8lx", reg_content); break;
+		case EBP:  thread_state.ebp    = reg_content; PDBG("EBP = %8lx", reg_content); break;
+		case ESI:  thread_state.esi    = reg_content; PDBG("ESI = %8lx", reg_content); break;
+		case EDI:  thread_state.edi    = reg_content; PDBG("EDI = %8lx", reg_content); break;
+		case EIP:  thread_state.ip     = reg_content; PDBG("EIP = %8lx", reg_content); break;
+		case EFL:  thread_state.eflags = reg_content; PDBG("EFL = %8lx", reg_content); break;
+		case CS:   PDBG("cannot change CS register"); PDBG(" CS = %8lx", reg_content); break;
+		case SS:   PDBG("cannot change SS register"); PDBG(" SS = %8lx", reg_content); break;
+		case DS:   PDBG("cannot change DS register"); PDBG(" DS = %8lx", reg_content); break;
+		case ES:   PDBG("cannot change ES register"); PDBG(" ES = %8lx", reg_content); break;
+		case FS:   thread_state.fs     = reg_content; PDBG(" FS = %8lx", reg_content); break;
+		case GS:   thread_state.gs     = reg_content; PDBG(" GS = %8lx", reg_content); break;
+
+	}
+
+	set_current_thread_state(thread_state);
+}
+
