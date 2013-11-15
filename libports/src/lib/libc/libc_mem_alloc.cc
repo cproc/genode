0a53380d53288351aac47026f5774a56fca5976b
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-11-15 22:40:46 +0100

    libc_mem_alloc.diff

--- libports/src/lib/libc/libc_mem_alloc.cc
+++ libports/src/lib/libc/libc_mem_alloc.cc
@@ -146,7 +146,7 @@ int Libc::Mem_alloc_impl::Dataspace_pool::expand(size_t size, Range_allocator *a
 {
 	Ram_dataspace_capability new_ds_cap;
 	void *local_addr, *ds_addr = 0;
-
+PDBG("size = %zu", size);
 	/* make new ram dataspace available at our local address space */
 	try {
 		new_ds_cap = _ram_session->alloc(size);
@@ -160,13 +160,13 @@ int Libc::Mem_alloc_impl::Dataspace_pool::expand(size_t size, Range_allocator *a
 
 	/* add new local address range to our local allocator */
 	alloc->add_range((addr_t)local_addr, size);
-
+PDBG("adding range %zx - %zx", (addr_t)local_addr, (addr_t)local_addr + size - 1);
 	/* now that we have new backing store, allocate Dataspace structure */
 	if (alloc->alloc_aligned(sizeof(Dataspace), &ds_addr, 2).is_error()) {
 		PWRN("could not allocate meta data - this should never happen");
 		return -1;
 	}
-
+PDBG("ds_addr = %p, sizeof(Dataspace) = %zu", ds_addr, sizeof(Dataspace));
 	/* add dataspace information to list of dataspaces */
 	Dataspace *ds  = new (ds_addr) Dataspace(new_ds_cap, local_addr);
 	insert(ds);
@@ -187,10 +187,11 @@ void *Libc::Mem_alloc_impl::alloc(size_t size, size_t align_log2)
 
 	/*
 	 * Calculate block size of needed backing store. The block must hold the
-	 * requested 'size' and a new Dataspace structure if the allocation above
-	 * failed. Finally, we align the size to a 4K page.
+	 * requested 'size' with the requested alignment and a new Dataspace
+	 * structure if the allocation above failed.
+	 * Finally, we align the size to a 4K page.
 	 */
-	size_t request_size = size + 1024;
+	size_t request_size = size + (1 << align_log2);
 
 	if (request_size < _chunk_size*sizeof(umword_t)) {
 		request_size = _chunk_size*sizeof(umword_t);
@@ -206,7 +207,7 @@ void *Libc::Mem_alloc_impl::alloc(size_t size, size_t align_log2)
 		PWRN("could not expand dataspace pool");
 		return 0;
 	}
-
+PDBG("size = %zu, align_log2 = %zu", size, align_log2);
 	/* allocate originally requested block */
 	return _alloc.alloc_aligned(size, &out_addr, align_log2).is_ok() ? out_addr : 0;
 }
