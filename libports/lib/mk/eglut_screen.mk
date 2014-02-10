4e1c991d03b6940faed95a968595eac80987398e
Christian Prochaska <christian.prochaska@genode-labs.com>
2014-02-10 19:05:50 +0100

    eglgears.diff

---
+++ libports/lib/mk/eglut_screen.mk
@@ -0,0 +1,13 @@
+include $(REP_DIR)/ports/mesa-demos.inc
+
+include $(REP_DIR)/lib/import/import-eglut_screen.mk
+
+EGLUT_DIR = $(REP_DIR)/contrib/$(MESA_DEMOS)/src/egl/eglut
+
+SRC_C = eglut.c eglut_screen.c
+
+LIBS += libc
+
+vpath %.c $(EGLUT_DIR)
+
+SHARED_LIB = yes
