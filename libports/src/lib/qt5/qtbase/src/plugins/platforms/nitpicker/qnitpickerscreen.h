c082bd0c863bc61e466ba7cef282fbc614e3b372
Christian Prochaska <christian.prochaska@genode-labs.com>
2014-02-20 16:32:58 +0100

    qt5_qpa_nitpicker.diff

--- libports/src/lib/qt5/qtbase/src/plugins/platforms/nitpicker/qnitpickerscreen.h
+++ libports/src/lib/qt5/qtbase/src/plugins/platforms/nitpicker/qnitpickerscreen.h
@@ -21,7 +21,6 @@
 
 /* Genode includes */
 #include <nitpicker_session/connection.h>
-#include <framebuffer_session/client.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -29,14 +28,12 @@ class QNitpickerScreen : public QPlatformScreen
 {
 	private:
 
-		Nitpicker::Connection       _nitpicker;
-		Framebuffer::Session_client _framebuffer;
+		Nitpicker::Connection _nitpicker;
 		QRect _geometry;
 
 	public:
 
 		QNitpickerScreen()
-		: _framebuffer(_nitpicker.framebuffer_session())
 		{
 			Framebuffer::Mode const scr_mode = _nitpicker.mode();
 			_nitpicker.buffer(scr_mode, false);
