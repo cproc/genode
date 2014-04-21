58d958e9626728b6abc2e59f049c5a15278c83be
Christian Prochaska <christian.prochaska@genode-labs.com>
2014-04-21 23:41:35 +0200

    tool_chain_gcc_makeinfo5.diff

--- ports/ports/gcc.mk
+++ ports/ports/gcc.mk
@@ -59,4 +59,3 @@ $(CONTRIB_DIR)/$(GCC)/configure:: $(CONTRIB_DIR)/$(GCC)
 	@# Noux-specific changes
 	@#
 	$(VERBOSE)patch -d $(CONTRIB_DIR)/$(GCC) -N -p1 < src/noux-pkg/gcc/build.patch
-	$(VERBOSE)patch -d $(CONTRIB_DIR)/$(GCC) -N -p1 < src/noux-pkg/gcc/build_with_makeinfo_5.patch
