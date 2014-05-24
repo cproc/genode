064298487fc0322d853de2bbb0b287895ad23ea0
Christian Prochaska <christian.prochaska@genode-labs.com>
2014-05-24 21:56:02 +0200

    qt5_new_ports_tools.diff

--- repos/libports/lib/mk/qt5_gui.mk
+++ repos/libports/lib/mk/qt5_gui.mk
@@ -32,10 +32,10 @@ qfiledialog.o: ui_qfiledialog.h
 include $(REP_DIR)/lib/mk/qt5.inc
 
 INC_DIR += $(REP_DIR)/include/qt5/qtbase/QtGui/private \
-           $(REP_DIR)/contrib/$(QT5)/qtbase/include/QtGui/$(QT_VERSION) \
-           $(REP_DIR)/contrib/$(QT5)/qtbase/include/QtGui/$(QT_VERSION)/QtGui \
-           $(REP_DIR)/contrib/$(QT5)/qtbase/include/QtGui/$(QT_VERSION)/QtGui/private \
-           $(REP_DIR)/contrib/$(QT5)/qtbase/include/QtCore/$(QT_VERSION) \
-           $(REP_DIR)/contrib/$(QT5)/qtbase/include/QtCore/$(QT_VERSION)/QtCore
+           $(QT5_CONTRIB_DIR)/qtbase/include/QtGui/$(QT_VERSION) \
+           $(QT5_CONTRIB_DIR)/qtbase/include/QtGui/$(QT_VERSION)/QtGui \
+           $(QT5_CONTRIB_DIR)/qtbase/include/QtGui/$(QT_VERSION)/QtGui/private \
+           $(QT5_CONTRIB_DIR)/qtbase/include/QtCore/$(QT_VERSION) \
+           $(QT5_CONTRIB_DIR)/qtbase/include/QtCore/$(QT_VERSION)/QtCore
 
 LIBS += qt5_core jpeg zlib libpng gallium
