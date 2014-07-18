71cfd8ddff201de53caecd7f757e8c2c9b8166e6
Christian Prochaska <christian.prochaska@genode-labs.com>
2014-07-18 19:07:46 +0200

    gdb_monitor_fixes.diff

--- repos/ports/src/app/gdb_monitor/cpu_root.h
+++ repos/ports/src/app/gdb_monitor/cpu_root.h
@@ -21,43 +21,46 @@
 #include <cpu_session_component.h>
 
 /* GDB monitor includes */
-#include "gdb_stub_thread.h"
+#include "genode_child_resources.h"
 
-namespace Gdb_monitor {
+namespace Gdb_monitor { class Cpu_root; }
 
-	class Cpu_root : public Root_component<Cpu_session_component>
-	{
-		private:
-			Gdb_stub_thread *_gdb_stub_thread;
+class Gdb_monitor::Cpu_root : public Root_component<Cpu_session_component>
+{
+	private:
 
-		protected:
+		Genode::Signal_receiver *_signal_receiver;
+		Genode_child_resources *_genode_child_resources;
 
-			Cpu_session_component *_create_session(const char *args)
-			{
-				Cpu_session_component *cpu_session_component =
-					new (md_alloc())
-					    Cpu_session_component(_gdb_stub_thread->exception_signal_receiver(), args);
-				_gdb_stub_thread->set_cpu_session_component(cpu_session_component);
-				return cpu_session_component;
-			}
+	protected:
 
-		public:
+		Cpu_session_component *_create_session(const char *args)
+		{
+			Cpu_session_component *cpu_session_component =
+				new (md_alloc())
+					Cpu_session_component(_signal_receiver, args);
+			_genode_child_resources->cpu_session_component(cpu_session_component);
+			return cpu_session_component;
+		}
 
-			/**
-			 * Constructor
-			 *
-			 * \param session_ep   entry point for managing cpu session objects
-			 * \param thread_ep    entry point for managing threads
-			 * \param md_alloc     meta data allocator to be used by root component
-			 */
-			Cpu_root(Rpc_entrypoint  *session_ep,
-					 Allocator       *md_alloc,
-					 Gdb_stub_thread *gdb_stub_thread)
-			:
-				Root_component<Cpu_session_component>(session_ep, md_alloc),
-				_gdb_stub_thread(gdb_stub_thread)
-			{ }
-	};
-}
+	public:
+
+		/**
+		 * Constructor
+		 *
+		 * \param session_ep   entry point for managing cpu session objects
+		 * \param thread_ep    entry point for managing threads
+		 * \param md_alloc     meta data allocator to be used by root component
+		 */
+		Cpu_root(Rpc_entrypoint *session_ep,
+				 Allocator *md_alloc,
+				 Genode::Signal_receiver *signal_receiver,
+				 Genode_child_resources *genode_child_resources)
+		:
+			Root_component<Cpu_session_component>(session_ep, md_alloc),
+			_signal_receiver(signal_receiver),
+			_genode_child_resources(genode_child_resources)
+		{ }
+};
 
 #endif /* _CPU_ROOT_H_ */
