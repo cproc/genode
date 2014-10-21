281b4aca4f2cc1c4580e825291cae158fab960ff
Christian Prochaska <christian.prochaska@genode-labs.com>
2014-10-21 18:00:20 +0200

    mem_test.diff

--- repos/base/src/test/printf/main.cc
+++ repos/base/src/test/printf/main.cc
@@ -1,22 +1,52 @@
-/*
- * \brief  Testing 'printf()' with negative integer
- * \author Christian Prochaska
- * \date   2012-04-20
- *
- */
-
-/*
- * Copyright (C) 2012-2013 Genode Labs GmbH
- *
- * This file is part of the Genode OS framework, which is distributed
- * under the terms of the GNU General Public License version 2.
- */
-
+#include <base/env.h>
 #include <base/printf.h>
 
-int main(int argc, char **argv)
+using namespace Genode;
+
+int main()
 {
-	Genode::printf("-1 = %d = %ld\n", -1, -1L);
+	unsigned int i;
+	unsigned int mismatch_start = 0;
+
+	enum { MEM_SIZE = 1024*1024*1024 };
+
+	Ram_dataspace_capability ds_cap = env()->ram_session()->alloc(MEM_SIZE);
+	char *mem = (char*)env()->rm_session()->attach(ds_cap);
+
+	if (!mem)
+		printf("malloc() failed\n");
+
+	printf("writing\n");
+	for (i = 0; i < MEM_SIZE; i++) {
+
+		mem[i] = 1;
+
+		register char c = mem[i];
+		if (c != 1)
+			printf("immediate mismatch\n");
+	}
+
+	printf("reading\n");
+	for (i = 0; i < MEM_SIZE; i++) {
+
+		register char c = mem[i];
+
+		if (c != 1) {
+
+			printf("mismatch at byte %u (%p): read %d\n", i, &mem[i], c);
+
+			if (mismatch_start == 0)
+				mismatch_start = i;
+
+		} else {
+
+			if (mismatch_start != 0) {
+				printf("mismatch range: %d bytes\n", i - mismatch_start);
+				mismatch_start = 0;
+			}
+
+		}
+	}
 
 	return 0;
 }
