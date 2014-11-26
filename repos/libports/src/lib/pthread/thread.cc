3584f5a1241eef93bd498bf89adf4591b01089c0
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2014-11-26 11:13:45 +0100

    pthread: support pthread_key_delete
    Issue #1296

--- repos/libports/src/lib/pthread/thread.cc
+++ repos/libports/src/lib/pthread/thread.cc
@@ -543,16 +543,15 @@ extern "C" {
 	};
 
 
+	static Lock key_list_lock;
 	List<Key_element> key_list[PTHREAD_KEYS_MAX];
-
 
 	int pthread_key_create(pthread_key_t *key, void (*destructor)(void*))
 	{
-		static Lock key_list_lock;
-		Lock_guard<Lock> key_list_lock_guard(key_list_lock);
-
 		if (!key)
 			return EINVAL;
+
+		Lock_guard<Lock> key_list_lock_guard(key_list_lock);
 
 		for (int k = 0; k < PTHREAD_KEYS_MAX; k++) {
 			/*
@@ -568,6 +567,20 @@ extern "C" {
 		}
 
 		return EAGAIN;
+	}
+
+
+	int pthread_key_delete(pthread_key_t key)
+	{
+		if (key < 0 || key >= PTHREAD_KEYS_MAX || key_list[key].first())
+			return EINVAL;
+
+		Lock_guard<Lock> key_list_lock_guard(key_list_lock);
+
+		while (Key_element * element = key_list[key].first())
+			key_list[key].remove(element);
+
+		return 0;
 	}
 
 
