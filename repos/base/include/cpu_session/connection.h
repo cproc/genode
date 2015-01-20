d6e6ca9ee197ea75084fd176a83ae30266fc661e
Christian Prochaska <christian.prochaska@genode-labs.com>
2015-01-20 17:34:54 +0100

    cpu_connection_quota.diff

--- repos/base/include/cpu_session/connection.h
+++ repos/base/include/cpu_session/connection.h
@@ -22,7 +22,7 @@ namespace Genode { struct Cpu_connection; }
 
 struct Genode::Cpu_connection : Connection<Cpu_session>, Cpu_session_client
 {
-	enum { RAM_QUOTA = 32*1024 };
+	enum { RAM_QUOTA = 48*1024 };
 
 	/**
 	 * Constructor
@@ -36,7 +36,7 @@ struct Genode::Cpu_connection : Connection<Cpu_session>, Cpu_session_client
 	               Affinity const &affinity = Affinity())
 	:
 		Connection<Cpu_session>(
-			session(affinity, "priority=0x%lx, ram_quota=36K, label=\"%s\"",
+			session(affinity, "priority=0x%lx, ram_quota=48K, label=\"%s\"",
 			        priority, label)),
 		Cpu_session_client(cap()) { }
 };
