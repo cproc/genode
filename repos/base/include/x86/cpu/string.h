2fed8354445e59cd502afa8e0aeb8aacc8a46832
Josef Söntgen <josef.soentgen@genode-labs.com>
2015-02-25 15:27:17 +0100

    highly optimized memcpy for x86

--- repos/base/include/x86/cpu/string.h
+++ repos/base/include/x86/cpu/string.h
@@ -25,8 +25,14 @@ namespace Genode
 	 *
 	 * \return      Number of bytes not copied
 	 */
-	inline size_t memcpy_cpu(void *, const void *, size_t size) {
-		return size; }
+	inline size_t memcpy_cpu(void *dst, const void *src, size_t size)
+	{
+		if (size & 1) asm volatile ("movsb" : "+D"(dst), "+S"(src) : : "memory");
+		if (size & 2) asm volatile ("movsw" : "+D"(dst), "+S"(src) : : "memory");
+		size /= 4;
+		asm volatile ("rep movsl" : "+D"(dst), "+S"(src), "+c" (size) : : "memory");
+		return 0;
+	}
 }
 
 #endif /* _INCLUDE__X86__CPU__STRING_H_ */
