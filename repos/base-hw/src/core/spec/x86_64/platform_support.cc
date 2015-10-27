40ebb51bf6a1dd06f44b515cc6c6ff91f70a55d0
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2015-10-27 14:26:40 +0100

    hw_x86: discover all physical memory in core
    Fixes #1741

--- repos/base-hw/src/core/spec/x86_64/platform_support.cc
+++ repos/base-hw/src/core/spec/x86_64/platform_support.cc
@@ -19,14 +19,29 @@
 
 using namespace Genode;
 
+extern "C" Genode::addr_t __initial_bx;
+
+
 Native_region * Platform::_core_only_mmio_regions(unsigned const i)
 {
 	static Native_region _regions[] =
 	{
 		{ Board::MMIO_LAPIC_BASE,  Board::MMIO_LAPIC_SIZE  },
 		{ Board::MMIO_IOAPIC_BASE, Board::MMIO_IOAPIC_SIZE },
+		{ 0, 0}
 	};
-	return i < sizeof(_regions)/sizeof(_regions[0]) ? &_regions[i] : 0;
+
+	unsigned const max = sizeof(_regions) / sizeof(_regions[0]) - 1;
+
+	if (i < max)
+		return &_regions[i];
+
+	if (i > max)
+		return 0;
+
+	_regions[i] = { __initial_bx & ~0xFFFUL, 4096 };
+
+	return &_regions[i];
 }
 
 
