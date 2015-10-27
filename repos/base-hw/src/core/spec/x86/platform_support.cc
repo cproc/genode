552c5e0c752971135e255a56c5710c75c8cb4b1d
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2015-10-27 17:16:55 +0100

    fixup "hw_x86: discover all physical memory in core"
    Issue #1741

--- repos/base-hw/src/core/spec/x86/platform_support.cc
+++ repos/base-hw/src/core/spec/x86/platform_support.cc
@@ -19,6 +19,7 @@
 
 #include <multiboot.h>
 
+/* contains physical pointer to multiboot */
 extern "C" Genode::addr_t __initial_bx;
 
 using namespace Genode;
@@ -27,33 +28,29 @@ Native_region * Platform::_ram_regions(unsigned const i)
 {
 	static Native_region _regions[16];
 
-	Genode::Multiboot_info amb(__initial_bx);
-
-	Multiboot_info::Mmap v = amb.phys_ram(i);
+	Multiboot_info::Mmap v = Genode::Multiboot_info(__initial_bx).phys_ram(i);
 	if (!v.base)
 		return nullptr;
 
-	Genode::uint64_t base = v.read<Multiboot_info::Mmap::Addr>();
-	Genode::uint64_t size = v.read<Multiboot_info::Mmap::Length>();
+	Multiboot_info::Mmap::Addr::access_t base = v.read<Multiboot_info::Mmap::Addr>();
+	Multiboot_info::Mmap::Length::access_t size = v.read<Multiboot_info::Mmap::Length>();
 
-	if (i >= sizeof(_regions) / sizeof(_regions[0])) {
-		PWRN("physical ram region 0x%llx+0x%llx will be not used", base, size);
-		return nullptr;
-	}
+	unsigned const max = sizeof(_regions) / sizeof(_regions[0]);
 
-	if (!_regions[i].size) {
-		if (base == 0 && size >= 4096) {
+	if (i < max && _regions[i].size == 0) {
+		if (base == 0 && size >= get_page_size()) {
 			/*
 			 * Exclude first physical page, so that it will become part of the
 			 * MMIO allocator. The framebuffer requests this page as MMIO.
 			 */
-			base  = 4096;
-			size -= 4096;
+			base  = get_page_size();
+			size -= get_page_size();
 		}
 		_regions[i] = { base, size };
-	}
+	} else if (i >= max)
+		PWRN("physical ram region 0x%llx+0x%llx will be not used", base, size);
 
-	return &_regions[i];
+	return i < max ? &_regions[i] : nullptr;
 }
 
 
