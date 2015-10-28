0beaa4cd30c2c78686a8ec89cdef8975e05aeb29
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2015-10-28 11:06:23 +0100

    fixup "base: support parsing multiboot for phys. memory"
    keep static physical ram detection for hw_muen
    
    Issue #1741

--- repos/base-hw/src/core/spec/x86/platform_support.cc
+++ repos/base-hw/src/core/spec/x86/platform_support.cc
@@ -17,42 +17,7 @@
 #include <board.h>
 #include <cpu.h>
 
-#include <multiboot.h>
-
-/* contains physical pointer to multiboot */
-extern "C" Genode::addr_t __initial_bx;
-
 using namespace Genode;
-
-Native_region * Platform::_ram_regions(unsigned const i)
-{
-	static Native_region _regions[16];
-
-	Multiboot_info::Mmap v = Genode::Multiboot_info(__initial_bx).phys_ram(i);
-	if (!v.base)
-		return nullptr;
-
-	Multiboot_info::Mmap::Addr::access_t base = v.read<Multiboot_info::Mmap::Addr>();
-	Multiboot_info::Mmap::Length::access_t size = v.read<Multiboot_info::Mmap::Length>();
-
-	unsigned const max = sizeof(_regions) / sizeof(_regions[0]);
-
-	if (i < max && _regions[i].size == 0) {
-		if (base == 0 && size >= get_page_size()) {
-			/*
-			 * Exclude first physical page, so that it will become part of the
-			 * MMIO allocator. The framebuffer requests this page as MMIO.
-			 */
-			base  = get_page_size();
-			size -= get_page_size();
-		}
-		_regions[i] = { base, size };
-	} else if (i >= max)
-		PWRN("physical ram region 0x%llx+0x%llx will be not used", base, size);
-
-	return i < max ? &_regions[i] : nullptr;
-}
-
 
 void Platform::_init_io_port_alloc()
 {
