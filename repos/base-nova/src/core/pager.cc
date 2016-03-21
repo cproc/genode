17cd608751e2e1e2c7a6888f93c52d273bc397bc
Christian Prochaska <christian.prochaska@genode-labs.com>
2016-03-21 16:08:47 +0100

    pause_resume_debug.diff

--- repos/base-nova/src/core/pager.cc
+++ repos/base-nova/src/core/pager.cc
@@ -196,14 +196,18 @@ void Pager_object::_recall_handler(addr_t pager_obj)
 		utcb->flags &= ~0x100UL;
 
 	/* deliver signal if it was requested */
-	if (obj->_state.to_submit())
+	if (obj->_state.to_submit()) {
+		PDBG("submitting signal");
 		obj->submit_exception_signal();
+	}
 
 	/* block until cpu_session()->resume() respectively wake_up() call */
 
 	unsigned long sm = obj->_state.blocked() ? obj->sel_sm_block() : 0;
 
 	obj->_state_lock.unlock();
+
+	PDBG("recall handler");
 
 	utcb->set_msg_word(0);
 	reply(myself->stack_top(), sm);
@@ -220,12 +224,6 @@ void Pager_object::_startup_handler(addr_t pager_obj)
 	utcb->sp  = obj->_initial_esp;
 
 	utcb->mtd = Mtd::EIP | Mtd::ESP;
-
-	if (obj->_state.singlestep()) {
-		utcb->flags = 0x100UL;
-		utcb->mtd |= Mtd::EFL;
-	}
-
 	utcb->set_msg_word(0);
 
 	reply(myself->stack_top());
@@ -347,12 +345,12 @@ void Pager_object::_invoke_handler(addr_t pager_obj)
 
 void Pager_object::wake_up()
 {
+	PDBG("wake_up()");
+
 	Lock::Guard _state_lock_guard(_state_lock);
 
 	if (!_state.blocked())
 		return;
-
-	_state.thread.exception = false;
 
 	_state.unblock();
 
