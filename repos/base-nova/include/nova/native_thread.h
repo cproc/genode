1364551004b681758c3ad1749e12dafee3c7ff64
Christian Prochaska <christian.prochaska@genode-labs.com>
2016-04-21 20:05:55 +0200

    nova_changes_for_gdb_monitor.diff

--- repos/base-nova/include/nova/native_thread.h
+++ repos/base-nova/include/nova/native_thread.h
@@ -31,12 +31,14 @@ struct Genode::Native_thread
 	addr_t ec_sel;     /* selector for execution context */
 	addr_t exc_pt_sel; /* base of event portal window */
 	bool   is_vcpu;
+	addr_t initial_ip; /* initial IP of local thread */
 
 	/* receive window for capability selectors received at the server side */
 	Receive_window rcv_window;
 
 	Native_thread() : ec_sel(INVALID_INDEX),
-	                  exc_pt_sel(INVALID_INDEX), is_vcpu(false) { }
+	                  exc_pt_sel(INVALID_INDEX), is_vcpu(false),
+	                  initial_ip(0) { }
 };
 
 #endif /* _INCLUDE__NOVA__NATIVE_THREAD_H_ */
