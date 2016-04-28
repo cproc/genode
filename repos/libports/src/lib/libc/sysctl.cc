2e7e3de583d1333e84bbae8b25e244e6489f7d28
Emery Hemingway <emery@vfemail.net>
2016-04-28 14:13:16 +0200

    fixup "libc: read sysctl info from /.sysctl/..."
    Revert sysctlbyname back to a dummy.
    Make memory related values fast and non-overridable.
    Default hostname is "localhost".
    
    Issue #1931

--- repos/libports/src/lib/libc/sysctl.cc
+++ repos/libports/src/lib/libc/sysctl.cc
@@ -54,9 +54,8 @@ extern "C" long sysconf(int name)
 extern "C" int sysctlbyname(char const *name, void *oldp, size_t *oldlenp,
                             void *newp, size_t newlen)
 {
-	PLOG("%s not implemented (name=%s)", __func__, name);
-	errno = ENOENT;
-	return -1;
+	PWRN("%s(%s,...) not implemented", __func__, name);
+	return Libc::Errno(ENOENT);
 }
 
 
@@ -70,25 +69,19 @@ extern "C" int __sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp,
 	static const ctlname ctl_p1003_1b_names[] = CTL_P1003_1B_NAMES;
 
 	/* read only */
-	if (!oldp || (*oldlenp < sizeof(int))) {
+	if (!oldp || (*oldlenp < sizeof(unsigned long))) {
 		/* check for write attempt */
 		errno = newp ? EPERM : EINVAL;
 		return -1;
 	}
 
-	Libc::Absolute_path sysctl_path("/.sysctl/");
-
 	ctlname const *ctl = nullptr;
 	char *buf = (char*)oldp;
 	int index_a = name[0];
 	int index_b = name[1];
-	int fd;
 
 	if (namelen != 2) return Libc::Errno(ENOENT);
 	if (index_a >= CTL_MAXID) return Libc::Errno(EINVAL);
-
-	sysctl_path.append(ctl_names[index_a].ctl_name);
-	sysctl_path.append("/");
 
 	switch(index_a) {
 	case CTL_KERN:
@@ -109,40 +102,84 @@ extern "C" int __sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp,
 	}
 	if (!ctl) return Libc::Errno(EINVAL);
 
-	sysctl_path.append(ctl->ctl_name);
 
-	/*
-	 * read from /.sysctl/...
-	 *
-	 * The abstracted libc interface is used to read files here
-	 * rather than to explicity resolve a file system plugin.
-	 */
-	fd = open(sysctl_path.base(), 0);
-	if (fd != -1) {
+	/* builtins */
+	{
+		switch(index_a) {
+		case CTL_HW: switch(index_b) {
 
-		auto n = read(fd, buf, *oldlenp);
-		close(fd);
+			case HW_PHYSMEM:
+			case HW_USERMEM:
+				*(unsigned long*)oldp = Genode::env()->ram_session()->quota();
+				*oldlenp = sizeof(unsigned long);
+				return 0;
 
-		if (n > 0) switch (ctl->ctl_type) {
-		case CTLTYPE_INT: {
-			long value = 0;
-			Genode::ascii_to((char*)oldp, value);
-			*(int*)oldp = int(value);
-			*oldlenp = sizeof(int);
-			return 0;
+			case HW_PAGESIZE:
+				*(int*)oldp = PAGESIZE;
+				*oldlenp = sizeof(int);
+				return 0;
+
+			} break;
+
+		case CTL_P1003_1B: switch(index_b) {
+
+			case CTL_P1003_1B_PAGESIZE:
+				*(int*)oldp = PAGESIZE;
+				*oldlenp = sizeof(int);
+				return 0;
+
+			} break;
 		}
-
-		case CTLTYPE_STRING:
-			*oldlenp = n;
-			return 0;
-
-		default:
-			PERR("unhandle sysctl data type for %s", sysctl_path.base());
-			return Libc::Errno(EINVAL);
-		}
-
 	}
-	/* fallthru to some baked-in defaults */
+
+
+	/* runtime overrides */
+	{
+		Libc::Absolute_path sysctl_path(ctl_names[index_a].ctl_name, "/.sysctl/");
+		sysctl_path.append("/");
+		sysctl_path.append(ctl->ctl_name);
+
+		/*
+		 * read from /.sysctl/...
+		 *
+		 * The abstracted libc interface is used to read files here
+		 * rather than to explicity resolve a file system plugin.
+		 */
+		int fd = open(sysctl_path.base(), 0);
+		if (fd != -1) {
+			auto n = read(fd, buf, *oldlenp);
+			close(fd);
+
+			if (n > 0) switch (ctl->ctl_type) {
+			case CTLTYPE_INT: {
+				long value = 0;
+				Genode::ascii_to((char*)oldp, value);
+				*(int*)oldp = int(value);
+				*oldlenp = sizeof(int);
+				return 0;
+			}
+
+			case CTLTYPE_ULONG: {
+				unsigned long value = 0;
+				Genode::ascii_to((char*)oldp, value);
+				*(unsigned long*)oldp = value;
+				*oldlenp = sizeof(unsigned long);
+				return 0;
+			}
+
+			case CTLTYPE_STRING:
+				*oldlenp = n;
+				return 0;
+
+			default:
+				PERR("unhandle sysctl data type for %s", sysctl_path.base());
+				return Libc::Errno(EINVAL);
+			}
+		}
+	}
+
+
+	/* fallback values */
 	{
 		switch(index_a) {
 
@@ -169,8 +206,8 @@ extern "C" int __sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp,
 				return 0;
 
 			case KERN_HOSTNAME:
-				*buf = '\0';
-				*oldlenp = 1;
+				Genode::strncpy(buf, "localhost", *oldlenp);
+				*oldlenp = Genode::strlen(buf);
 				return 0;
 
 			} break;
@@ -187,37 +224,13 @@ extern "C" int __sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp,
 				*oldlenp = sizeof(int);
 				return 0;
 
-			case HW_PHYSMEM:
-				*(unsigned long*)oldp = Genode::env()->ram_session()->quota();
-				*oldlenp = sizeof(unsigned long);
-				return 0;
-
-			case HW_PAGESIZE:
-				*(int*)oldp = PAGESIZE;
-				*oldlenp = sizeof(int);
-				return 0;
-
-			case HW_MODEL:
-			case HW_MACHINE_ARCH:
-				Genode::strncpy(buf, "unknown", *oldlenp);
-				*oldlenp = Genode::strlen(buf);
-				return 0;
-
-			} break;
-
-		case CTL_P1003_1B: switch(index_b) {
-
-			case CTL_P1003_1B_PAGESIZE:
-				*(int*)oldp = 4096;
-				*oldlenp = sizeof(int);
-				return 0;
-
 			} break;
 
 		}
 	}
 
-	PWRN("sysctl %s not found", sysctl_path.base());
+	PWRN("sysctl: no builtin or override value found for %s.%s",
+	     ctl_names[index_a].ctl_name, ctl->ctl_name);
 	return Libc::Errno(ENOENT);
 }
 
