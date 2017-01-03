eb9215649b41e940847b53503159fd2f3c480429
Emery Hemingway <emery@vfemail.net>
2017-01-03 15:28:09 +0100

    merge lib/config_args with lib/posix
    Merging the config_args library with the POSIX library supplies
    'argc' and 'argv' arguments to components using a 'main' entry.
    
    Fix #2218
    Ref #1987

--- repos/libports/src/lib/posix/construct.cc
+++ repos/libports/src/lib/posix/construct.cc
@@ -28,5 +28,44 @@ extern "C" int main(int argc, char ** argv, char **envp);
 
 void Libc::Component::construct(Libc::Env &env)
 {
+	using Genode::Xml_node;
+	using Genode::Xml_attribute;
+
+	env.config([&] (Xml_node const &node) {
+		int argc = 0;
+		static char **argv;
+
+		/* count the number of arguments */
+		node.for_each_sub_node("arg", [&] (Xml_node const &arg_node) {
+			/* check if the 'value' attribute exists */
+			if (arg_node.has_attribute("value"))
+				++argc;
+		});
+
+		if (argc == 0)
+			return;
+
+		argv = (char**)env.heap().alloc((argc + 1) * sizeof(char*));
+
+		/* read the arguments */
+		int i = 0;
+		node.for_each_sub_node("arg", [&] (Xml_node const &arg_node) {
+			try {
+				Xml_attribute attr = arg_node.attribute("value");
+				Genode::size_t const arg_len = attr.value_size()+1;
+				argv[i] = (char*)env.heap().alloc(arg_len);
+				attr.value(argv[i], arg_len);
+				++i;
+			} catch (Xml_node::Nonexistent_sub_node) { }
+		});
+
+		argv[i] = nullptr;
+
+		/* register command-line arguments at Genode's startup code */
+		genode_argc = argc;
+		genode_argv = argv;
+	});
+
+
 	exit(main(genode_argc, genode_argv, genode_envp));
 }
