7509b695dddaafdb61bc10d942dcbd26b35fb768
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-02-27 18:45:00 +0100

    fixup: 'usb/rpi: fix inconsistency between C and C++'

--- repos/dde_linux/src/lib/usb/spec/rpi/platform.cc
+++ repos/dde_linux/src/lib/usb/spec/rpi/platform.cc
@@ -251,12 +251,6 @@ extern "C" void dwc_otg_fiq_fsm(struct fiq_state *state, int num_channels) { TRA
 
 unsigned char _dwc_otg_fiq_stub, _dwc_otg_fiq_stub_end;
 
-/*
- * CAUTION: Within the OTG driver these of type bool (C: _BOOL), we give them
- * one byte room here
- */
-extern char fiq_enable, fiq_fsm_enable;
-
 
 /***********************
  ** asm/dma_mapping.h **
