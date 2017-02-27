593410c4c46bfe0324ff99dead5a8fdc00adf0e1
Christian Prochaska <christian.prochaska@genode-labs.com>
2017-02-27 20:20:58 +0100

    fixup "gdb_monitor: kernel-agnostic build support" (cleanup)
    Issue #2292

--- repos/ports/lib/mk/spec/x86_32/gdbserver_platform-nova.mk
+++ repos/ports/lib/mk/spec/x86_32/gdbserver_platform-nova.mk
@@ -1,7 +1,10 @@
 REQUIRES += nova
 
-SRC_CC = spec/nova_x86_32/low.cc native_cpu.cc
+SRC_CC = spec/x86_32/low.cc \
+         native_cpu.cc
 
 SHARED_LIB = yes
+
+vpath %.cc $(REP_DIR)/src/lib/gdbserver_platform-nova
 
 include $(REP_DIR)/lib/mk/spec/x86_32/gdbserver_platform-x86_32.inc
