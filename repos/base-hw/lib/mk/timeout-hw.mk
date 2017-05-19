8e9dfa8b95ccdbbd7e9cd12f8e732384f036b40e
Martin Stein <martin.stein@genode-labs.com>
2017-05-19 14:30:53 +0200

    fixup "os/timer: interpolate time via timestamps" (move to base lib)
    To enable us to use the timeout lib as back end in the
    Timer::Connection, it must be a dependency of the base lib to also be
    available in statically linked binaries. As the base lib is a dependency
    of the LDSO lib too, we can remove the timeout dependency from the LDSO
    lib.
    
    Ref #2400

--- repos/base-hw/lib/mk/timeout-hw.mk
+++ repos/base-hw/lib/mk/timeout-hw.mk
@@ -3,4 +3,8 @@ SRC_CC += timer.cc
 SRC_CC += hw/timer_timestamp.cc
 SRC_CC += duration.cc
 
+LIBS += alarm
+
+INC_DIR += $(BASE_DIR)/src/include
+
 vpath % $(BASE_DIR)/../os/src/lib/timeout
--- repos/base-nova/lib/mk/spec/x86_64/ld-nova.mk
+++ repos/base-nova/lib/mk/spec/x86_64/ld-nova.mk
@@ -1,3 +1,3 @@
-BASE_LIBS += base-nova-common base-nova timeout
+BASE_LIBS += base-nova-common base-nova
 
 include $(BASE_DIR)/lib/mk/spec/x86_64/ld-platform.inc
