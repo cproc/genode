5a7feb7bc3e4fe80cfbb2c59cb22e4072ca5614b
Christian Helmuth <christian.helmuth@genode-labs.com>
2017-11-02 14:41:41 +0100

    Revert "sel4: use kernel branch 7.0 + our mastered commits"
    This reverts commit 1248a34a6a21ccf81773fa8efbce85a4c597f74c.

--- repos/base-sel4/lib/import/import-syscall-sel4.mk
+++ repos/base-sel4/lib/import/import-syscall-sel4.mk
@@ -10,3 +10,6 @@ INC_DIR += $(SEL4_INCLUDE_DIR)
 # Access to other sel4-specific headers such as 'autoconf.h'.
 #
 INC_DIR += $(SEL4_INCLUDE_DIR)/sel4
+
+# required for seL4_DebugPutChar
+CC_OPT += -DCONFIG_PRINTING
