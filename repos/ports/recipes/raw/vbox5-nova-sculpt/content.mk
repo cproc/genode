248b9eb1eb2c083abbb0a9f4fbdb4617b8ac25b6
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-03-09 18:46:20 +0100

    sculpt_vbox5.diff

--- repos/ports/recipes/raw/vbox5-nova-sculpt/content.mk
+++ repos/ports/recipes/raw/vbox5-nova-sculpt/content.mk
@@ -1,4 +1,7 @@
-content: init.config
+content: init.config nit_fb.config
 
 init.config:
+	cp $(REP_DIR)/recipes/raw/vbox5-nova-sculpt/$@ $@
+
+nit_fb.config:
 	cp $(REP_DIR)/recipes/raw/vbox5-nova-sculpt/$@ $@
