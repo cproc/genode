313df6c18a5d051a8e0fb56e08d48e15ddba67db
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-03-26 16:27:18 +0200

    qt5_qpluginwidget_test.diff

--- repos/libports/src/test/qt5/qpluginwidget/target.mk
+++ repos/libports/src/test/qt5/qpluginwidget/target.mk
@@ -1,10 +1,6 @@
-# identify the QT5 repository by searching for a file that is unique for QT5
-QT5_REP_DIR := $(call select_from_repositories,lib/import/import-qt5.inc)
-QT5_REP_DIR := $(realpath $(dir $(QT5_REP_DIR))../..)
+include $(call select_from_repositories,src/app/qt5/tmpl/target_defaults.inc)
 
-include $(QT5_REP_DIR)/src/app/qt5/tmpl/target_defaults.inc
-
-include $(QT5_REP_DIR)/src/app/qt5/tmpl/target_final.inc
+include $(call select_from_repositories,src/app/qt5/tmpl/target_final.inc)
 
 LIBS += qt5_qpluginwidget qt5_network qoost
 
