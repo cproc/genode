762d0a1dfbba6e18361a6756d465df66e5f98d00
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-03-27 13:40:55 +0200

    qt5_qpluginwidget_test.diff

--- repos/libports/lib/import/import-qt5_qpluginwidget.mk
+++ repos/libports/lib/import/import-qt5_qpluginwidget.mk
@@ -1,8 +1,4 @@
-ifeq ($(CONTRIB_DIR),)
 QT5_INC_DIR += $(call select_from_repositories,include/qt5/qpluginwidget)
-else
-QT5_INC_DIR += $(REP_DIR)/include/qt5/qpluginwidget
-endif
 
 # 'qpluginwidget.h' includes 'qnitpickerviewwidget.h'
 include $(call select_from_repositories,lib/import/import-qt5_qnitpickerviewwidget.mk)
