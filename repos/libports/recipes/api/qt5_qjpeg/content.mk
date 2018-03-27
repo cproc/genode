e6bd0f334c725b1f54a578369d662fd3a9691024
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-03-27 13:38:07 +0200

    qt5_qjpeg_recipe.diff

---
+++ repos/libports/recipes/api/qt5_qjpeg/content.mk
@@ -0,0 +1,18 @@
+# XXX: remove qt5_version.inc?
+
+MIRROR_FROM_REP_DIR := lib/import/import-qt5_qjpeg.mk \
+                       lib/import/import-qt5.inc \
+                       lib/symbols/qt5_qjpeg \
+                       lib/mk/qt5_version.inc
+
+content: $(MIRROR_FROM_REP_DIR)
+
+$(MIRROR_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
+PORT_DIR := $(call port_dir,$(REP_DIR)/ports/qt5)
+
+content: LICENSE
+
+LICENSE:
+	cp $(PORT_DIR)/src/lib/qt5/qt-everywhere-opensource-src-5.8.0/LICENSE.LGPLv3 $@
