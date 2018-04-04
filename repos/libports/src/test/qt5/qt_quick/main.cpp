4850ca80da2781005bb41ca8499fd4b1307cf996
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-04-04 12:16:22 +0200

    qt5_quicktest_recipe.diff

--- repos/libports/src/test/qt5/qt_quick/main.cpp
+++ repos/libports/src/test/qt5/qt_quick/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQuickView view;
-    view.setSource(QUrl("qrc:/qt_quicktest.qml"));
+    view.setSource(QUrl("qrc:/qt_quick.qml"));
     view.show();
 
     return app.exec();
