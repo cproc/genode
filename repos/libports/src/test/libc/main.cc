f8f5699c383acd48025d4dd35788120edc988a97
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-07-09 11:22:02 +0200

    libc_fd_alloc_debug.diff

--- repos/libports/src/test/libc/main.cc
+++ repos/libports/src/test/libc/main.cc
@@ -21,6 +21,7 @@
 #include <base/env.h>
 
 /* libC includes */
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,109 +34,10 @@ int main(int argc, char **argv)
 {
 	printf("--- libC test ---\n");
 
-	printf("Does printf work?\n");
-	printf("We can find out by printing a floating-point number: %f. How does that work?\n", 1.2345);
-
-
-	enum { ROUNDS = 64, SIZE_LARGE = 2048 };
-
-	unsigned error_count = 0;
-
-	printf("Malloc: check small sizes\n");
-	for (size_t size = 1; size < SIZE_LARGE; size = 2*size + 3) {
-		void *addr[ROUNDS];
-		for (unsigned i = 0; i < ROUNDS; ++i) {
-			addr[i] = malloc(size);
-
-			if (!((unsigned long)addr[i] & 0xf))
-				continue;
-
-			printf("%u. malloc(%zu) returned addr = %p - ERROR\n", i, size, addr[i]);
-			++error_count;
-		}
-		for (unsigned i = 0; i < ROUNDS; ++i) free(addr[i]);
+	for (;;) {
+		int fd = open("/dev/zero", O_RDONLY);
+		Genode::log("fd: ", fd);
 	}
 
-	printf("Malloc: check larger sizes\n");
-	for (size_t size = SIZE_LARGE; size < 1024*1024; size = 2*size + 15) {
-		void *addr[ROUNDS];
-		for (unsigned i = 0; i < ROUNDS; ++i) {
-			addr[i] = malloc(size);
-
-			if (!((unsigned long)addr[i] & 0xf))
-				continue;
-
-			printf("%u. malloc(%zu) returned addr = %p - ERROR\n", i, size, addr[i]);
-			++error_count;
-		}
-		for (unsigned i = 0; i < ROUNDS; ++i) free(addr[i]);
-	}
-
-	printf("Malloc: check realloc\n");
-	{
-		void *addr = malloc(32);
-		memset(addr, 13, 32);
-
-		for (unsigned i = 0; i < ROUNDS; ++i) {
-			size_t const size = 32 + 11*i;
-			char *a = (char *)realloc(addr, size);
-			if (memcmp(addr, a, 32) || a[32] != 0) {
-				printf("realloc data error");
-				++error_count;
-			}
-			addr = a;
-
-			if (!((unsigned long)addr & 0xf))
-				continue;
-
-			printf("%u. realloc(%zu) returned addr = %p - ERROR\n", i, size, addr);
-			++error_count;
-		}
-		for (int i = ROUNDS - 1; i >= 0; --i) {
-			size_t const size = 32 + 11*i;
-			char *a = (char *)realloc(addr, size);
-			if (memcmp(addr, a, 32) || a[32] != 0) {
-				printf("realloc data error");
-				++error_count;
-			}
-			addr = a;
-
-			if (!((unsigned long)addr & 0xf))
-				continue;
-
-			printf("%u. realloc(%zu) returned addr = %p - ERROR\n", i, size, addr);
-			++error_count;
-		}
-	}
-
-	printf("Malloc: check really large allocation\n");
-	for (unsigned i = 0; i < 4; ++i) {
-		size_t const size = 250*1024*1024;
-		void *addr = malloc(size);
-		free(addr);
-
-		if ((unsigned long)addr & 0xf) {
-			printf("large malloc(%zu) returned addr = %p - ERROR\n", size, addr);
-			++error_count;
-		}
-	}
-
-	{
-		pid_t const tid = syscall(SYS_thr_self);
-		if (tid == -1) {
-			printf("syscall(SYS_thr_self) returned %d (%s) - ERROR\n", tid, strerror(errno));
-			++error_count;
-		} else {
-			printf("syscall(SYS_thr_self) returned %d\n", tid);
-		}
-		int const ret = syscall(0xffff);
-		if (ret != -1) {
-			printf("syscall(unknown) returned %d - ERROR\n", ret);
-			++error_count;
-		} else {
-			printf("syscall(unknown) returned %d (%s)\n", ret, strerror(errno));
-		}
-	}
-
-	exit(error_count);
+	return 0;
 }
