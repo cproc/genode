cb37eba7a533ac763a2ac7a27d91c5e7e0071faa
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-10-12 17:41:08 +0200

    linux_test.diff

---
+++ linux_test/func2/func.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void func2()
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+	{
+		if (i % 3 == 0)
+			printf ("%d is divisible by 3\n", i);
+		if (i % 11 == 0)
+			printf ("%d is divisible by 11\n", i);
+	}
+}
