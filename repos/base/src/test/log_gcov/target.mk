646f16e0e31ef1f15c9692d0efb5f8d565f23a93
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-11-05 15:14:10 +0100

    test.diff

---
+++ repos/base/src/test/log_gcov/target.mk
@@ -0,0 +1,6 @@
+TARGET = test-log_gcov
+SRC_CC = main.cc func.cc
+LIBS   = base libgcov
+
+#CC_OPT += -fprofile-arcs -ftest-coverage -fprofile-dir='/test/log'
+CC_OPT += -fprofile-arcs -ftest-coverage
