33317c06efc9675fc5b7c9c7d498a15216c12b65
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-11-05 16:12:14 +0100

    libgcov.diff

---
+++ repos/libports/src/lib/gcov/string.h
@@ -0,0 +1,9 @@
+#ifndef STRING_H
+#define STRING_H
+
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+char *strcpy(char *dest, const char *src);
+size_t strlen(const char *s);
+
+#endif
