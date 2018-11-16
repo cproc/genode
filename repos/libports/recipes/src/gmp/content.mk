7c48825dbe831632428d00d4f0a8060f83b5af54
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-11-16 14:18:44 +0100

    fixup_gmp_package.diff

--- repos/libports/recipes/src/gmp/content.mk
+++ repos/libports/recipes/src/gmp/content.mk
@@ -9,7 +9,13 @@ MIRROR_FROM_REP_DIR = \
 	lib/mk/spec/x86_32/gmp-mpn.mk \
 	lib/mk/spec/x86_64/gmp-mpn.mk \
 	
-content: $(MIRROR_FROM_REP_DIR) LICENSE include src/lib/gmp
+content: $(MIRROR_FROM_REP_DIR) \
+         LICENSE \
+         include \
+         include/gmp \
+         include/spec/32bit/gmp \
+         include/spec/64bit/gmp \
+         src/lib/gmp
 
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
@@ -20,8 +26,17 @@ PORT_SRC_DIR := $(PORT_DIR)/src/lib/gmp
 include:
 	mkdir -p $@
 	cp -r $(PORT_DIR)/include/* $@/
+
+include/gmp: include
+	mkdir -p $@
 	cp -r $(REP_DIR)/include/gmp/* $@/
+
+include/spec/32bit/gmp: include
+	mkdir -p $@
 	cp -r $(REP_DIR)/include/spec/32bit/gmp/* $@/
+
+include/spec/64bit/gmp: include
+	mkdir -p $@
 	cp -r $(REP_DIR)/include/spec/64bit/gmp/* $@/
 
 src/lib/gmp:
