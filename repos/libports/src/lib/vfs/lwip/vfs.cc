6bf22ff3a5a0d92ed62520800cfc16764b4f06f1
Christian Prochaska <christian.prochaska@genode-labs.com>
2019-01-10 15:54:53 +0100

    socket fs: support non-blocking 'connect()'

--- repos/libports/src/lib/vfs/lwip/vfs.cc
+++ repos/libports/src/lib/vfs/lwip/vfs.cc
@@ -1162,7 +1162,11 @@ class Lwip::Tcp_socket_dir final :
 				break;
 
 			case Lwip_file_handle::CONNECT:
-				return !ip_addr_isany(&_pcb->remote_ip);
+				/*
+				 * The connect file is considered readable when the socket is
+				 * writeable (connected or error).
+				 */
+				return ((state == READY) || (state == CLOSED));
 
 			case Lwip_file_handle::LOCATION:
 			case Lwip_file_handle::LOCAL:
@@ -1303,6 +1307,15 @@ class Lwip::Tcp_socket_dir final :
 				break;
 
 			case Lwip_file_handle::CONNECT:
+				switch (state) {
+				case READY:
+					out_count = Genode::snprintf(dst, count, "connected");
+					break;
+				default:
+					out_count = Genode::snprintf(dst, count, "connection refused");
+					break;
+				}
+				return Read_result::READ_OK;
 			case Lwip_file_handle::LISTEN:
 			case Lwip_file_handle::INVALID: break;
 			}
