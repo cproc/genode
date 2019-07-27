9f2d3be0c2a00c44e93efeed7b40504267187b1d
Christian Prochaska <christian.prochaska@genode-labs.com>
2019-07-27 02:32:13 +0200

    qt5_vfs_pipe.diff

--- repos/libports/lib/mk/qt5_core.mk
+++ repos/libports/lib/mk/qt5_core.mk
@@ -29,6 +29,6 @@ include $(REP_DIR)/lib/mk/qt5.inc
 SRC_CC += libc_dummies.cc
 vpath libc_dummies.cc $(REP_DIR)/src/lib/qt5
 
-LIBS += zlib pcre16 libc libm libc_pipe
+LIBS += zlib pcre16 libc libm
 
 CC_CXX_WARN_STRICT =
