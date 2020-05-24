ea2adfbdfc9a8ebaed5c5aa247c24d9a9e95060c
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-05-25 01:37:52 +0200

    qt5_recipes.diff

--- repos/libports/src/app/qt5/examples/calculatorform/target.mk
+++ repos/libports/src/app/qt5/examples/calculatorform/target.mk
@@ -2,7 +2,8 @@ include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
 
 TARGET = calculatorform
 
-LIBS = qt5_base stdcxx
+LIBS = base libc libm mesa qt5_component stdcxx
+LIBS += libQt5Core libQt5Gui libQt5Widgets
 
 $(TARGET): env.sh qmake_root
 
@@ -10,9 +11,15 @@ $(TARGET): env.sh qmake_root
 	@# add Qt dependencies to qmake_root
 	@#
 
+ifeq ($(CONTRIB_DIR),)
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Core/libQt5Core.abi.so       qmake_root/lib/libQt5Core.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Gui/libQt5Gui.abi.so         qmake_root/lib/libQt5Gui.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Widgets/libQt5Widgets.abi.so qmake_root/lib/libQt5Widgets.lib.so
+else
 	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Core.lib.so qmake_root/lib/
 	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Gui.lib.so qmake_root/lib/
 	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Widgets.lib.so qmake_root/lib/
+endif
 
 	@#
 	@# run qmake
--- repos/libports/src/app/qt5/examples/minimal_browser/target.mk
+++ repos/libports/src/app/qt5/examples/minimal_browser/target.mk
@@ -2,11 +2,11 @@ include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
 
 TARGET = minimal
 
-#
-# build dependency libraries in var/libcache
-#
-
-LIBS = qt5_base qt5_declarative qt5_quickcontrols qt5_webengine stdcxx
+LIBS = base libc libm mesa qt5_component stdcxx
+LIBS += libQt5Core libQt5Gui libQt5Network libQt5Widgets
+LIBS += libQt5Qml libQt5Quick libQt5QuickWidgets
+LIBS += qt5_quickcontrols
+LIBS += libQt5WebEngineCore libQt5WebEngine
 
 $(TARGET): env.sh qmake_root
 
@@ -14,14 +14,25 @@ $(TARGET): env.sh qmake_root
 	@# add Qt dependencies to qmake_root
 	@#
 
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Core.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Gui.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Network.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Qml.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Quick.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Widgets.lib.so qmake_root/lib/
+ifeq ($(CONTRIB_DIR),)
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Core/libQt5Core.abi.so                   qmake_root/lib/libQt5Core.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Gui/libQt5Gui.abi.so                     qmake_root/lib/libQt5Gui.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Network/libQt5Network.abi.so             qmake_root/lib/libQt5Network.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Qml/libQt5Qml.abi.so                     qmake_root/lib/libQt5Qml.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Quick/libQt5Quick.abi.so                 qmake_root/lib/libQt5Quick.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5Widgets/libQt5Widgets.abi.so             qmake_root/lib/libQt5Widgets.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5WebEngineCore/libQt5WebEngineCore.abi.so qmake_root/lib/libQt5WebEngineCore.lib.so
+	ln -sf $(BUILD_BASE_DIR)/var/libcache/libQt5WebEngine/libQt5WebEngine.abi.so         qmake_root/lib/libQt5WebEngine.lib.so
+else
+	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Core.lib.so          qmake_root/lib/
+	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Gui.lib.so           qmake_root/lib/
+	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Network.lib.so       qmake_root/lib/
+	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Qml.lib.so           qmake_root/lib/
+	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Quick.lib.so         qmake_root/lib/
+	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Widgets.lib.so       qmake_root/lib/
 	ln -sf $(BUILD_BASE_DIR)/bin/libQt5WebEngineCore.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5WebEngine.lib.so qmake_root/lib/
+	ln -sf $(BUILD_BASE_DIR)/bin/libQt5WebEngine.lib.so     qmake_root/lib/
+endif
 
 	@#
 	@# run qmake
