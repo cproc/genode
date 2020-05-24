519e5dd6a84365eb15f8aa5cd9383ece5ebe4266
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-05-25 01:37:52 +0200

    qt5_recipes.diff

--- repos/libports/src/app/qt5/examples/calculatorform/target.mk
+++ repos/libports/src/app/qt5/examples/calculatorform/target.mk
@@ -1,47 +1,9 @@
-include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
-
 TARGET = calculatorform
 
-LIBS = qt5_base stdcxx
+QMAKE_PROJECT_FILE = $(QT_DIR)/qttools/examples/designer/calculatorform/calculatorform.pro
 
-$(TARGET): env.sh qmake_root
+QT5_LIBS = libQt5Core libQt5Gui libQt5Widgets
 
-	@#
-	@# add Qt dependencies to qmake_root
-	@#
+LIBS = libc libm mesa qt5_component stdcxx $(QT5_LIBS)
 
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Core.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Gui.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Widgets.lib.so qmake_root/lib/
-
-	@#
-	@# run qmake
-	@#
-
-	source env.sh && $(QMAKE) \
-		-qtconf qmake_root/mkspecs/genode-x86-g++/qt.conf \
-		$(QT_DIR)/qttools/examples/designer/calculatorform/calculatorform.pro
-
-	@#
-	@# build
-	@#
-
-	source env.sh && $(MAKE)
-
-	@#
-	@# create stripped version
-	@#
-
-	$(STRIP) $(TARGET) -o $(TARGET).stripped
-
-	@#
-	@# create symlink in 'bin' directory
-	@#
-
-	ln -sf $(CURDIR)/$(TARGET).stripped $(PWD)/bin/$(TARGET)
-
-	@#
-	@# create symlink in 'debug' directory
-	@#
-
-	ln -sf $(CURDIR)/$(TARGET) $(PWD)/debug/
+include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
--- repos/libports/src/app/qt5/examples/minimal_browser/target.mk
+++ repos/libports/src/app/qt5/examples/minimal_browser/target.mk
@@ -1,56 +1,11 @@
-include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
-
 TARGET = minimal
 
-#
-# build dependency libraries in var/libcache
-#
+QMAKE_PROJECT_FILE = $(QT_DIR)/qtwebengine/examples/webengine/minimal/minimal.pro
 
-LIBS = qt5_base qt5_declarative qt5_quickcontrols qt5_webengine stdcxx
+QT5_LIBS += libQt5Core libQt5Gui libQt5Network
+QT5_LIBS += libQt5Qml libQt5Quick
+QT5_LIBS += libQt5WebEngineCore libQt5WebEngine
 
-$(TARGET): env.sh qmake_root
+LIBS = libc libm mesa qt5_component stdcxx qt5_quickcontrols $(QT5_LIBS)
 
-	@#
-	@# add Qt dependencies to qmake_root
-	@#
-
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Core.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Gui.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Network.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Qml.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Quick.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5Widgets.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5WebEngineCore.lib.so qmake_root/lib/
-	ln -sf $(BUILD_BASE_DIR)/bin/libQt5WebEngine.lib.so qmake_root/lib/
-
-	@#
-	@# run qmake
-	@#
-
-	source env.sh && $(QMAKE) \
-		-qtconf qmake_root/mkspecs/genode-x86-g++/qt.conf \
-		$(QT_DIR)/qtwebengine/examples/webengine/minimal/minimal.pro
-
-	@#
-	@# build
-	@#
-
-	source env.sh && $(MAKE)
-
-	@#
-	@# create stripped version
-	@#
-
-	$(STRIP) $(TARGET) -o $(TARGET).stripped
-
-	@#
-	@# create symlink in 'bin' directory
-	@#
-
-	ln -sf $(CURDIR)/$(TARGET).stripped $(PWD)/bin/$(TARGET)
-
-	@#
-	@# create symlink in 'debug' directory
-	@#
-
-	ln -sf $(CURDIR)/$(TARGET) $(PWD)/debug/
+include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
