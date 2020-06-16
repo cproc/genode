4332efc1a8e1bddaf91a7c46399b79b233a1a6b5
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-16 18:42:37 +0200

    minimal_browser.diff

---
+++ repos/libports/src/app/qt5/examples/minimal_browser/target.mk
@@ -0,0 +1,13 @@
+TARGET = build_with_qmake
+
+QMAKE_PROJECT_FILE = $(QT_DIR)/qtwebengine/examples/webengine/minimal/minimal.pro
+
+QMAKE_TARGET_BINARIES = minimal
+
+QT5_PORT_LIBS += libQt5Core libQt5Gui libQt5Network
+QT5_PORT_LIBS += libQt5Qml libQt5Quick
+QT5_PORT_LIBS += libQt5WebEngineCore libQt5WebEngine
+
+LIBS = libc libm mesa qt5_component stdcxx qt5_quickcontrols $(QT5_PORT_LIBS)
+
+include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
