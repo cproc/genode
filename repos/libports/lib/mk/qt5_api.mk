bb37cb5f93a8d19f60655ce4faa420b8186f36a7
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-27 23:34:42 +0200

    qt5_api_mk_qtwebengine.diff

--- repos/libports/lib/mk/qt5_api.mk
+++ repos/libports/lib/mk/qt5_api.mk
@@ -27,10 +27,30 @@ built.tag: qmake_prepared.tag qmake_root/lib/ld.lib.so
 	-skip qtserialport \
 	-skip qttools \
 	-skip qtwebglplugin \
-	-skip qtwebengine \
 	-no-feature-dbus \
 	-no-feature-networkinterface \
-	-no-feature-process
+	-no-feature-process \
+	-no-webengine-alsa \
+	-no-webengine-embedded-build \
+	-qt-webengine-icu \
+	-qt-webengine-ffmpeg \
+	-qt-webengine-opus \
+	-qt-webengine-webp \
+	-no-webengine-pepper-plugins \
+	-no-webengine-printing-and-pdf \
+	-no-webengine-proprietary-codecs \
+	-no-webengine-pulseaudio \
+	-no-webengine-spellchecker \
+	-no-webengine-native-spellchecker \
+	-no-webengine-extensions \
+	-no-webengine-webrtc \
+	-no-webengine-geolocation \
+	-no-webengine-v8-snapshot \
+	-no-webengine-webchannel \
+	-no-webengine-kerberos \
+	-webengine-widgets \
+	-webengine-qml \
+	-no-webengine-sndio
 
 	@#
 	@# build
