c975f1b6bb6574a2933c7006a73b237bc75ad675
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-06-27 23:09:00 +0200

    qt5_api_mk.diff

---
+++ repos/libports/lib/mk/qt5_api.mk
@@ -0,0 +1,50 @@
+include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
+
+LIBS  = libc libm stdcxx qt5_component egl mesa
+LIBS += base qoost
+
+built.tag: qmake_prepared.tag qmake_root/lib/ld.lib.so
+
+	@#
+	@# run configure
+	@#
+
+	source env.sh && $(QT_DIR)/configure \
+	-prefix /qt \
+	-xplatform $(QMAKE_PLATFORM) \
+	-qpa genode \
+	-opensource \
+	-confirm-license \
+	-no-pch \
+	-release \
+	-force-debug-info \
+	-no-strip \
+	-opengl desktop \
+	-nomake tools \
+	-skip qt3d \
+	-skip qtlocation \
+	-skip qtmultimedia \
+	-skip qtsensors \
+	-skip qtserialport \
+	-skip qttools \
+	-skip qtwebglplugin \
+	-skip qtwebengine \
+	-no-feature-dbus \
+	-no-feature-networkinterface \
+	-no-feature-process
+
+	@#
+	@# build
+	@#
+
+	source env.sh && $(MAKE)
+
+	#
+	# install into local 'install' directory
+	#
+
+	$(MAKE) INSTALL_ROOT=$(CURDIR)/install install
+
+ifeq ($(called_from_lib_mk),yes)
+all: built.tag
+endif
