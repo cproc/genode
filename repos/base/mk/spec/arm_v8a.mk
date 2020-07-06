6a25fbbab2bb976ea47a84123c66dfb18a38eb7d
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-07-06 12:49:47 +0200

    minimal_browser.diff

--- repos/base/mk/spec/arm_v8a.mk
+++ repos/base/mk/spec/arm_v8a.mk
@@ -2,6 +2,6 @@ SPECS       += arm_v8 arm_64 64bit
 REP_INC_DIR += include/spec/arm_v8
 REP_INC_DIR += include/spec/arm_64
 
-CC_MARCH    ?= -march=armv8-a
+CC_MARCH    ?= -march=armv8-a+crc+crypto
 
 include $(BASE_DIR)/mk/spec/64bit.mk
