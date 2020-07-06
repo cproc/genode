a1ce2f80c4e73bc9cb57e2365f0bda4b358d7c6d
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-07-06 12:49:47 +0200

    minimal_browser.diff

--- repos/libports/src/lib/qt5_component/qt_component.cc
+++ repos/libports/src/lib/qt5_component/qt_component.cc
@@ -23,16 +23,32 @@
 /* provided by the application */
 extern "C" int main(int argc, char const **argv);
 
+extern "C" void wait_for_continue();
 void Libc::Component::construct(Libc::Env &env)
 {
 	Libc::with_libc([&] {
 
 		qpa_init(env);
 
+#if 1
+		int argc = 7;
+		char const *argv[] = { "qt5_app",
+		                       "--single-process",
+		                       "--disable-gpu",
+		                       "--enable-logging",
+		                       "--log-level=3",
+		                       "--v=0",
+		                       "--no-sandbox",
+		                       0 };
+#else
 		int argc = 1;
-		char const *argv[] = { "qt5_app", 0 };
-
+		char const *argv[] = { "qt5_app",
+		                       0 };
+#endif
+//wait_for_continue();
 		int exit_value = main(argc, argv);
+
+//Genode::error("main() returned");
 
 		exit(exit_value);
 	});
