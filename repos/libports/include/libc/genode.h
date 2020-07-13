1a62c41721c8a057c1dbb5bff687d8d945ac0bf3
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-07-13 16:11:48 +0200

    cache_coherent.diff

---
+++ repos/libports/include/libc/genode.h
@@ -0,0 +1,25 @@
+/*
+ * \brief  POSIX wrappers for important base lib features
+ * \author Christian Prochaska
+ * \date   2020-07-06
+ */
+
+/*
+ * Copyright (C) 2020 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+/* libc includes */
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void genode_cache_coherent(void *addr, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
