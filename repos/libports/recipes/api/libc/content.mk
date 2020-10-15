9c2b6e345d84363e45927a793df6530cf5fcde9e
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-10-15 17:24:37 +0200

    libc_api.diff

--- repos/libports/recipes/api/libc/content.mk
+++ repos/libports/recipes/api/libc/content.mk
@@ -17,6 +17,7 @@ include:
 	cp -r $(REP_DIR)/include/libc $@/
 	cp -r $(REP_DIR)/include/libc-genode $@/
 	cp    $(REP_DIR)/src/lib/libc/internal/legacy.h $@/libc/
+	cp -r $(REP_DIR)/include/libc-plugin $@/
 
 content: lib/mk/ldso_so_support.mk src/lib/ldso/so_support.c
 
