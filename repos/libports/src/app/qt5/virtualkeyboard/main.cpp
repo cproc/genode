7a5692f7eb787b4f72df426bdeb7485484c190f7
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-11-20 19:11:35 +0100

    qt5_virtualkeyboard.diff

---
+++ repos/libports/src/app/qt5/virtualkeyboard/main.cpp
@@ -0,0 +1,141 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt Virtual Keyboard module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:GPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 or (at your option) any later version
+** approved by the KDE Free Qt Foundation. The licenses are as published by
+** the Free Software Foundation and appearing in the file LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QQuickView>
+#include <QQuickWidget>
+#include <QQuickItem>
+#include <QApplication>
+#include <QDebug>
+
+static constexpr bool debug = true;
+
+class Im_enabled_item : public QQuickItem
+{
+	Q_OBJECT
+
+	private:
+
+		QFile       _event_file;
+		QTextStream _event_stream;
+
+	protected:
+
+		QVariant inputMethodQuery(Qt::InputMethodQuery query) const override
+		{
+			/*
+			 * Without preferring lower case, the shift key will be
+			 * pushed at startup.
+			 */
+
+			if (query == Qt::ImHints)
+				return Qt::ImhPreferLowercase;
+
+			return QQuickItem::inputMethodQuery(query);
+		}
+
+		void focusInEvent(QFocusEvent *event) override
+		{
+			QQuickItem::focusInEvent(event);
+
+			/*
+			 * This is needed to get the 'Qt::ImhPreferLowercase'
+			 * flag applied (seen in QQuickTextInput).
+			 */
+
+			qGuiApp->inputMethod()->show();
+		}
+
+
+		void keyReleaseEvent(QKeyEvent *e) override
+		{
+			if (debug)
+				qDebug() << "Im_enabled_item::keyReleaseEvent(): " << e;
+
+			if (e->text() != "") {
+				_event_stream << e->text();
+				_event_stream.flush();
+			} else if (e->key() == Qt::Key_Backspace) {
+				_event_stream << QString("\x08");
+				_event_stream.flush();
+			}
+		}
+
+#if 0
+		bool event(QEvent *e) override
+		{
+			qDebug() << this << ": event(): " << e;
+			return QQuickItem::event(e);
+		}
+#endif
+
+	public:
+
+		Im_enabled_item(QQuickItem *parent = nullptr)
+		: QQuickItem(parent),
+		  _event_file("/event/text"),
+		  _event_stream(&_event_file)
+		{
+			setFlag(ItemAcceptsInputMethod, true);
+
+			if (!_event_file.open(QIODevice::WriteOnly))
+				qFatal("ERROR: could not open event VFS file");
+
+			_event_stream.setCodec("UTF-8");
+		}
+
+};
+
+
+int main(int argc, char *argv[])
+{
+	qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+
+	QApplication app(argc, argv);
+
+	qmlRegisterType<Im_enabled_item>("Im_enabled_item", 1, 0, "Im_enabled_item");
+
+	QQuickWidget view(QString("qrc:/virtualkeyboard.qml"));
+
+	view.setResizeMode(QQuickWidget::SizeRootObjectToView);
+
+	view.resize(1024, 400);
+
+	view.show();
+	
+	/*
+	 * Trigger a virtual keyboard shift handler reset to enable the shift
+	 * key.
+	 */
+	view.setFocus();
+	view.activateWindow();
+
+	return app.exec();
+}
+
+#include "main.moc"
