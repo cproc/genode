07d759b8e3dd423f0ba94bed90feaa48c4542a4a
Christian Prochaska <christian.prochaska@genode-labs.com>
2021-05-05 01:35:28 +0200

    tool_chain_gdb_9_2.diff

--- repos/ports/src/app/gdb_monitor/target.mk
+++ repos/ports/src/app/gdb_monitor/target.mk
@@ -4,10 +4,10 @@ GDB_CONTRIB_DIR = $(call select_from_ports,gdb)/src/noux-pkg/gdb
 
 INC_DIR += $(GDB_CONTRIB_DIR)/include \
            $(GDB_CONTRIB_DIR)/gdb \
-           $(GDB_CONTRIB_DIR)/gdb/common \
+           $(GDB_CONTRIB_DIR)/gdb/gdbsupport \
            $(GDB_CONTRIB_DIR)/gdb/gdbserver \
-           $(GDB_CONTRIB_DIR)/gdb/gnulib/import \
            $(GDB_CONTRIB_DIR)/gdb/regformats \
+           $(GDB_CONTRIB_DIR)/gnulib/import \
            $(REP_DIR)/src/lib/gdbserver_libc_support \
            $(PRG_DIR)/gdbserver \
            $(PRG_DIR)
@@ -18,36 +18,37 @@ LIBS    =  stdcxx libc \
 # libiberty
 SRC_C   = argv.c \
           concat.c \
-          crc32.c
+          crc32.c \
+          xstrdup.c
 
 # gnulib
 SRC_C  += rawmemchr.c \
           strchrnul.c
 
 # gdb
-SRC_CC += common/agent.cc \
-          common/buffer.cc \
-          common/cleanups.cc \
-          common/common-debug.cc \
-          common/common-exceptions.cc \
-          common/common-utils.cc \
-          common/environ.cc \
-          common/errors.cc \
-          common/filestuff.cc \
-          common/format.cc \
-          common/gdb_tilde_expand.cc \
-          common/gdb_vecs.cc \
-          common/job-control.cc \
-          common/netstuff.cc \
-          common/pathstuff.cc \
-          common/posix-strerror.cc \
-          common/print-utils.cc \
-          common/ptid.cc \
-          common/rsp-low.cc \
-          common/signals.cc \
-          common/tdesc.cc \
-          common/vec.cc \
-          common/xml-utils.cc \
+SRC_CC += gdbsupport/agent.cc \
+          gdbsupport/buffer.cc \
+          gdbsupport/cleanups.cc \
+          gdbsupport/common-debug.cc \
+          gdbsupport/common-exceptions.cc \
+          gdbsupport/common-inferior.cc \
+          gdbsupport/common-utils.cc \
+          gdbsupport/environ.cc \
+          gdbsupport/errors.cc \
+          gdbsupport/filestuff.cc \
+          gdbsupport/format.cc \
+          gdbsupport/gdb_tilde_expand.cc \
+          gdbsupport/gdb_vecs.cc \
+          gdbsupport/job-control.cc \
+          gdbsupport/netstuff.cc \
+          gdbsupport/pathstuff.cc \
+          gdbsupport/print-utils.cc \
+          gdbsupport/ptid.cc \
+          gdbsupport/rsp-low.cc \
+          gdbsupport/safe-strerror.cc \
+          gdbsupport/signals.cc \
+          gdbsupport/tdesc.cc \
+          gdbsupport/xml-utils.cc \
           gdbserver/ax.cc \
           gdbserver/debug.cc \
           gdbserver/dll.cc \
@@ -71,7 +72,8 @@ SRC_CC += common/agent.cc \
           nat/fork-inferior.cc \
           nat/linux-ptrace.cc \
           nat/x86-dregs.cc \
-          target/waitstatus.cc
+          target/waitstatus.cc \
+          alloc.cc
 
 # genode
 SRC_CC += genode-low.cc \
@@ -85,7 +87,7 @@ CC_OPT += -DGDBSERVER -DPKGVERSION="\"8.3.0\"" -DREPORT_BUGS_TO="\"\""
 CC_OPT += -DHAVE_SYS_WAIT_H -DHAVE_SYS_PTRACE_H -DHAVE_DECL_PTRACE -DHAVE_TERMIOS
 CC_OPT += -fpermissive -Wno-unused-function
 
-vpath %.c  $(GDB_CONTRIB_DIR)/gdb/gnulib/import
+vpath %.c  $(GDB_CONTRIB_DIR)/gnulib/import
 vpath %.c  $(GDB_CONTRIB_DIR)/libiberty
 vpath %.cc $(GDB_CONTRIB_DIR)/gdb
 vpath %.cc $(PRG_DIR)/gdbserver
