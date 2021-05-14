0ce1a441a8286f83495c2679eba45194b4599901
Christian Prochaska <christian.prochaska@genode-labs.com>
2021-05-14 15:49:07 +0200

    tmp_tool_chain_gdb_eb497a2a77219d9cd4d05aa32bd569f5ff40b266.diff

--- repos/ports/src/app/gdb_monitor/gdbserver/genode-low.cc
+++ repos/ports/src/app/gdb_monitor/gdbserver/genode-low.cc
@@ -302,7 +302,7 @@ pid_t my_waitpid(pid_t pid, int *status, int flags)
 
 				if (cc == 1 && c == '\003' && current_thread != NULL) {
 					/* this causes a SIGINT to be delivered to one of the threads */
-					(*the_target->request_interrupt)();
+					the_target->pt->request_interrupt();
 					continue;
 				} else {
 					if (verbose)
