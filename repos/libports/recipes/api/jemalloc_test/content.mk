edc978868b01f15bc36e06c980f5415c5a00061a
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-02-06 22:19:11 +0100

    jemalloc_tests.diff

---
+++ repos/libports/recipes/api/jemalloc_test/content.mk
@@ -0,0 +1,14 @@
+content: include lib/symbols/jemalloc_test LICENSE
+
+PORT_DIR := $(call port_dir,$(REP_DIR)/ports/jemalloc)
+
+include:
+	mkdir -p $@
+#	cp -r $(PORT_DIR)/include/jemalloc/* $@/
+	cp -r $(REP_DIR)/include/jemalloc $@/
+
+lib/symbols/jemalloc_test:
+	$(mirror_from_rep_dir)
+
+LICENSE:
+	cp $(PORT_DIR)/src/lib/jemalloc/COPYING $@
