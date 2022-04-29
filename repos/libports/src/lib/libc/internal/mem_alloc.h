3a9ca2f7000de04de522ffc720b64e4c42a70424
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-04-29 21:15:05 +0200

    fork_mmap.diff

--- repos/libports/src/lib/libc/internal/mem_alloc.h
+++ repos/libports/src/lib/libc/internal/mem_alloc.h
@@ -20,6 +20,7 @@
 #include <base/env.h>
 #include <util/list.h>
 #include <rm_session/rm_session.h>
+#include <internal/clone_session.h>
 
 /* libc-internal includes */
 #include <internal/types.h>
@@ -54,9 +55,10 @@ namespace Libc {
 			{
 				Ram_dataspace_capability cap;
 				void *local_addr;
+				size_t size;
 
-				Dataspace(Ram_dataspace_capability c, void *a)
-				: cap(c), local_addr(a) {}
+				Dataspace(Ram_dataspace_capability c, void *a, size_t s)
+				: cap(c), local_addr(a), size(s) {}
 			};
 
 			class Dataspace_pool : public List<Dataspace>
@@ -98,6 +100,28 @@ namespace Libc {
 
 					void reassign_resources(Ram_allocator *ram, Region_map *rm) {
 						_ram = ram, _region_map = rm; }
+
+					void update_cloned_ds_cap(void *local_addr, Ram_dataspace_capability cap)
+					{
+						for (Dataspace *ds = first(); ds; ds = ds->next()) {
+							if (ds->local_addr == local_addr) {
+								/*
+								 * Clear the cloned capability without
+								 * triggering reference counting.
+								 */
+								construct_at<Ram_dataspace_capability>(&ds->cap);
+								ds->cap = cap;
+								return;
+							}
+						}
+					}
+
+					template <typename FN>
+					void for_each_region(FN const &fn) const {
+						for (Dataspace const *ds = first(); ds; ds = ds->next())  {
+							fn(ds->local_addr, (Genode::size_t)ds->size);
+						}
+					}
 			};
 
 			Mutex  mutable _mutex;
@@ -128,6 +152,19 @@ namespace Libc {
 			void *alloc(size_t size, size_t align_log2);
 			void free(void *ptr);
 			Size_at_result size_at(void const *ptr) const;
+
+			void reassign_resources(Ram_allocator *ram, Region_map *rm)
+			{ _ds_pool.reassign_resources(ram, rm); }
+
+			void update_cloned_ds_cap(void *local_addr, Ram_dataspace_capability cap)
+			{
+				_ds_pool.update_cloned_ds_cap(local_addr, cap);
+			}
+
+			template <typename FN>
+			void for_each_region(FN const &fn) const {
+				_ds_pool.for_each_region(fn);
+			}
 	};
 }
 
