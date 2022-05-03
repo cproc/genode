031b6e652e89ae39ef5568f7fe8be069a6288303
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-05-03 21:29:54 +0200

    jemalloc_weak_malloc.diff

--- repos/libports/src/lib/libc/pthread.cc
+++ repos/libports/src/lib/libc/pthread.cc
@@ -931,7 +931,8 @@ extern "C" {
 		if (!attr)
 			return EINVAL;
 
-		Libc::Allocator alloc { };
+		/* used during jemalloc initialization */
+		Libc::Old_allocator alloc { };
 		*attr = new (alloc) pthread_mutex_attr { PTHREAD_MUTEX_NORMAL };
 
 		return 0;
@@ -946,7 +947,7 @@ extern "C" {
 		if (!attr || !*attr)
 			return EINVAL;
 
-		Libc::Allocator alloc { };
+		Libc::Old_allocator alloc { };
 		destroy(alloc, *attr);
 		*attr = nullptr;
 
@@ -985,7 +986,8 @@ extern "C" {
 		if (*mutex != PTHREAD_MUTEX_INITIALIZER)
 			return 0;
 
-		Libc::Allocator alloc { };
+		/* used during jemalloc initialization */
+		Libc::Old_allocator alloc { };
 
 		pthread_mutextype const type = (!attr || !*attr)
 		                             ? PTHREAD_MUTEX_NORMAL : (*attr)->type;
@@ -1021,7 +1023,7 @@ extern "C" {
 		if ((!mutex) || (*mutex == PTHREAD_MUTEX_INITIALIZER))
 			return EINVAL;
 
-		Libc::Allocator alloc { };
+		Libc::Old_allocator alloc { };
 		destroy(alloc, *mutex);
 		*mutex = PTHREAD_MUTEX_INITIALIZER;
 
