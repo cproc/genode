4714c7e3c8770791b1c69712b8534f682d340189
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-06-05 04:36:49 +0200

    wip_qt5_cmake.diff

--- repos/libports/src/test/qt5/qpluginwidget/target.mk
+++ repos/libports/src/test/qt5/qpluginwidget/target.mk
@@ -1,12 +1,12 @@
-QMAKE_PROJECT_FILE = $(PRG_DIR)/qpluginwidget.pro
+CMAKE_LISTS_DIR = $(PRG_DIR)
 
-QMAKE_TARGET_BINARIES = test-qpluginwidget
+CMAKE_TARGET_BINARIES = test-qpluginwidget
 
 QT5_PORT_LIBS = libQt5Core libQt5Gui libQt5Network libQt5Widgets
 
 LIBS = libc libm mesa qt5_component stdcxx libqgenodeviewwidget libqpluginwidget qoost $(QT5_PORT_LIBS)
 
-include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
+include $(call select_from_repositories,lib/import/import-qt5_cmake.mk)
 
 #
 # create tar archive for test plugin
