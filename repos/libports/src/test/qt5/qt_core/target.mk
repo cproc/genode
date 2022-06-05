9d72493845fd344834fd486ce445f1d4595242a8
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-06-05 04:36:49 +0200

    tmp_qt5_cmake_tests.diff

--- repos/libports/src/test/qt5/qt_core/target.mk
+++ repos/libports/src/test/qt5/qt_core/target.mk
@@ -1,9 +1,9 @@
-QMAKE_PROJECT_FILE = $(PRG_DIR)/qt_core.pro
+CMAKE_LISTS_DIR = $(PRG_DIR)
 
-QMAKE_TARGET_BINARIES = test-qt_core
+CMAKE_TARGET_BINARIES = test-qt_core
 
 QT5_PORT_LIBS = libQt5Core
 
 LIBS = libc libm qt5_component stdcxx $(QT5_PORT_LIBS)
 
-include $(call select_from_repositories,lib/import/import-qt5_qmake.mk)
+include $(call select_from_repositories,lib/import/import-qt5_cmake.mk)
