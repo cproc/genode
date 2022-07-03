52c23df0db391e62742d0b98e8fd401ab81f391c
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-07-03 18:35:50 +0200

    jemalloc_weak_malloc.diff

--- repos/libports/include/libc/allocator.h
+++ repos/libports/include/libc/allocator.h
@@ -20,8 +20,13 @@
 /* libc includes */
 #include <stdlib.h>
 
-namespace Libc { struct Allocator; }
+namespace Libc {
+	struct Allocator;
+	struct Old_allocator;
+}
 
+extern "C" void *libc_malloc(size_t);
+extern "C" void libc_free(void *);
 
 struct Libc::Allocator : Genode::Allocator
 {
@@ -30,6 +35,20 @@ struct Libc::Allocator : Genode::Allocator
 	Alloc_result try_alloc(size_t size) override { return malloc(size); }
 
 	void free(void *addr, size_t size) override { ::free(addr); }
+
+	bool need_size_for_free() const override { return false; }
+
+	size_t overhead(size_t size) const override { return 0; }
+};
+
+
+struct Libc::Old_allocator : Genode::Allocator
+{
+	typedef Genode::size_t size_t;
+
+	Alloc_result try_alloc(size_t size) override { return libc_malloc(size); }
+
+	void free(void *addr, size_t size) override { libc_free(addr); }
 
 	bool need_size_for_free() const override { return false; }
 
--- repos/libports/src/lib/libc/pthread.cc
+++ repos/libports/src/lib/libc/pthread.cc
@@ -920,7 +920,8 @@ extern "C" {
 		if (!attr)
 			return EINVAL;
 
-		Libc::Allocator alloc { };
+		/* used during jemalloc initialization */
+		Libc::Old_allocator alloc { };
 		*attr = new (alloc) pthread_mutex_attr { PTHREAD_MUTEX_NORMAL };
 
 		return 0;
@@ -935,7 +936,7 @@ extern "C" {
 		if (!attr || !*attr)
 			return EINVAL;
 
-		Libc::Allocator alloc { };
+		Libc::Old_allocator alloc { };
 		destroy(alloc, *attr);
 		*attr = nullptr;
 
@@ -966,8 +967,8 @@ extern "C" {
 		if (!mutex)
 			return EINVAL;
 
-
-		Libc::Allocator alloc { };
+		/* used during jemalloc initialization */
+		Libc::Old_allocator alloc { };
 
 		pthread_mutextype const type = (!attr || !*attr)
 		                             ? PTHREAD_MUTEX_NORMAL : (*attr)->type;
@@ -994,7 +995,7 @@ extern "C" {
 		if ((!mutex) || (*mutex == PTHREAD_MUTEX_INITIALIZER))
 			return EINVAL;
 
-		Libc::Allocator alloc { };
+		Libc::Old_allocator alloc { };
 		destroy(alloc, *mutex);
 		*mutex = PTHREAD_MUTEX_INITIALIZER;
 
