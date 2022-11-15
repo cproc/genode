ebea4ac160ed8d020b7492a28f7931e35e7ec556
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-11-15 16:09:19 +0100

    libdrm/lima: disable use of pthread_mutex

--- repos/libports/src/lib/libdrm/ioctl_lima.cc
+++ repos/libports/src/lib/libdrm/ioctl_lima.cc
@@ -868,12 +868,14 @@ int lima_drm_munmap(void *addr)
 
 int lima_drm_poll(int fd)
 {
+#if 0
 	static pthread_mutex_t poll_mutex = PTHREAD_MUTEX_INITIALIZER;
 	int const err = pthread_mutex_lock(&poll_mutex);
 	if (err) {
 		Genode::error("could not lock poll mutex: ", err);
 		return -1;
 	}
+#endif
 
 	int const handle = fd - Lima::Call::SYNC_FD;
 	_drm->wait_for_syncobj((unsigned)handle);
