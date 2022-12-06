5b19396a7495aa6b3d6426a8c2826881a62cc148
Norman Feske <norman.feske@genode-labs.com>
2022-12-06 11:19:03 +0100

    fixup "vfs,libc: deferred wakeup of remote peers" (vfs_replay)

--- repos/gems/src/app/vfs_replay/component.cc
+++ repos/gems/src/app/vfs_replay/component.cc
@@ -35,10 +35,14 @@ class Vfs_replay
 		Vfs_replay(const Vfs_replay&) = delete;
 		Vfs_replay& operator=(const Vfs_replay&) = delete;
 
-
 		Env &_env;
 
+		using Vfs_peers = Vfs::Remote_io::Deferred_wakeups;
+
+
 		Vfs::File_system &_vfs;
+		Vfs_peers        &_vfs_peers;
+
 		Vfs::Vfs_handle  *_vfs_handle;
 
 		Attached_ram_dataspace _write_buffer;
@@ -421,6 +425,8 @@ class Vfs_replay
 			if (_finished) {
 				_env.parent().exit(failed ? 1 : 0);
 			}
+
+			_vfs_peers.trigger();
 		}
 
 		struct Io_response_handler : Vfs::Io_response_handler
@@ -441,11 +447,12 @@ class Vfs_replay
 
 	public:
 
-		Vfs_replay(Vfs::File_system &vfs, Env &env,
+		Vfs_replay(Env &env, Vfs::File_system &vfs, Vfs_peers &vfs_peers,
 		           Xml_node const & config)
 		:
 			_env { env },
 			_vfs { vfs },
+			_vfs_peers { vfs_peers },
 			_vfs_handle { nullptr },
 			_write_buffer { _env.ram(), _env.rm(),
 			                config.attribute_value("write_buffer_size", 1u << 20) },
@@ -513,7 +520,8 @@ struct Main : private Genode::Entrypoint::Io_progress_handler
 	Genode::Signal_handler<Main> _reactivate_handler {
 		_env.ep(), *this, &Main::handle_io_progress };
 
-	Vfs_replay _replay { _vfs_env.root_dir(), _env, _config_rom.xml() };
+	Vfs_replay _replay { _env, _vfs_env.root_dir(), _vfs_env.deferred_wakeups(),
+	                     _config_rom.xml() };
 
 	Main(Genode::Env &env) : _env { env }
 	{
