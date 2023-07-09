a527f5845bc956a3911764e2e2934fa699c78990
Christian Prochaska <christian.prochaska@genode-labs.com>
2023-07-10 01:56:14 +0200

    tmp_gdb_monitor_test_debug3.diff

--- repos/ports/src/test/gdb_monitor/main.cc
+++ repos/ports/src/test/gdb_monitor/main.cc
@@ -1,89 +1,24 @@
-/*
- * \brief  GDB Monitor test
- * \author Christian Prochaska
- * \date   2011-05-24
- */
-
-/*
- * Copyright (C) 2011-2019 Genode Labs GmbH
- *
- * This file is part of the Genode OS framework, which is distributed
- * under the terms of the GNU Affero General Public License version 3.
- */
-
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
-
-/* a variable to be modified with GDB */
-int test_var = 1;
-
-
-void test_thread_step()
+void *thread_func(void *)
 {
-	/* nothing */
+	for (;;) {
+		fprintf(stderr, "pthread\n");
+		sleep(1);
+	}
 }
-
-
-void test_thread_sigsegv()
-{
-	*(int *)0 = 42;
-}
-
-
-void *test_thread_start(void*)
-{
-//	test_thread_step();
-//	test_thread_sigsegv();
-	return nullptr;
-}
-
-
-/*
- * This function returns the current value of 'test_var' + 1 and can be called from
- * GDB using the 'call' or 'print' commands
- */
-int test_var_func()
-{
-	return test_var + 1;
-}
-
-/* this function returns a value to make itself appear in the stack trace when building with -O2 */
-int func2()
-{
-	/* set the first breakpoint here to test the 'backtrace' command for a
-	 * thread which is not in a syscall */
-	puts("in func2()\n");
-
-	/* call 'test_var_func()', so the compiler does not throw the function away */
-	printf("test_var_func() returned %d\n", test_var_func());
-
-	return 0;
-}
-
-
-/* this function returns a value to make itself appear in the stack trace when building with -O2 */
-int func1()
-{
-	func2();
-
-	return 0;
-}
-
 
 int main()
 {
-	printf("main()\n");
-
-//	func1();
+//	pthread_t t;
+//	pthread_create(&t, 0, thread_func, 0);
 
 	for (;;) {
-		pthread_t test_thread;
-		static int count = 0;
-printf("calling pthread_create(%d)\n", count++);
-		pthread_create(&test_thread, nullptr, test_thread_start, nullptr);
-		pthread_join(test_thread, nullptr);
-//		for (;;);
+		fprintf(stderr, "main\n");
+		sleep(1);
+		*(int *)0 = 42;
 	}
 
 	return 0;
