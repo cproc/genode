0f6370377e5cb3a7df71522b0fcd967270c3f938
Christian Prochaska <christian.prochaska@genode-labs.com>
2023-07-10 02:47:19 +0200

    wip_monitor.diff

--- repos/os/src/monitor/gdb_arch.h
+++ repos/os/src/monitor/gdb_arch.h
@@ -20,7 +20,11 @@
 
 namespace Monitor { namespace Gdb {
 
+	static constexpr char   breakpoint_instruction[] { '\xCC' };
+	static constexpr size_t breakpoint_len { sizeof(breakpoint_instruction) };
+
 	void print_registers(Output &out, Cpu_state const &cpu);
+	void scan_registers(Const_byte_range_ptr const &in, Cpu_state &cpu);
 } }
 
 #endif /* _GDB_ARCH_H_ */
