cbe76b1ecc3ec0f510d64b00d5fe8b2b77e4addf
Christian Prochaska <christian.prochaska@genode-labs.com>
2023-08-10 07:04:46 +0200

    fixup "hw: send exception signals and support single-stepping" (Cpu_state)
    Issue #4975

--- repos/base/include/spec/arm_64/cpu/cpu_state.h
+++ repos/base/include/spec/arm_64/cpu/cpu_state.h
@@ -27,10 +27,10 @@ struct Genode::Cpu_state
 		BREAKPOINT    = 0x3c,
 	};
 
-	uint64_t r[31]  { 0 }; /* general purpose register 0...30 */
-	uint64_t sp     { 0 }; /* stack pointer                   */
-	uint64_t ip     { 0 }; /* instruction pointer             */
-	uint64_t ec     { 0 }; /* exception class                 */
+	addr_t r[31] { 0 }; /* general purpose register 0...30 */
+	addr_t sp    { 0 }; /* stack pointer                   */
+	addr_t ip    { 0 }; /* instruction pointer             */
+	addr_t ec    { 0 }; /* exception class                 */
 };
 
 #endif /* _INCLUDE__SPEC__ARM_64__CPU__CPU_STATE_H_ */
