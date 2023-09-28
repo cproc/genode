a1aaf0deee46aac507a6d8d9ff14a18e5aaa4100
Benjamin Lamowski <benjamin.lamowski@genode-labs.com>
2023-09-28 14:58:15 +0200

    fixup: "base: rework vmm library API"
    Add author.
    
    Ref #4968

--- repos/base/src/lib/base/vm.cc
+++ repos/base/src/lib/base/vm.cc
@@ -2,11 +2,12 @@
  * \brief  Generic VM-connection implementation
  * \author Alexander Boettcher
  * \author Christian Helmuth
+ * \author Benjamin Lamowski
  * \date   2018-08-27
  */
 
 /*
- * Copyright (C) 2018-2021 Genode Labs GmbH
+ * Copyright (C) 2018-2023 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU Affero General Public License version 3.
