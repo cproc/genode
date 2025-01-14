4a7a35080a5fdd14887ff0e3dab0ed1b98ce3a4e
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-01-14 15:26:38 +0100

    TMP: how to use Qt6SvgWidgets with CMake

--- repos/libports/src/test/qt6/qt_core_cmake/main.cpp
+++ repos/libports/src/test/qt6/qt_core_cmake/main.cpp
@@ -5,8 +5,11 @@
  */
 
 #include <QtCore>
+#include <QtSvgWidgets/QSvgWidget>
 
 int main(int argc, char *argv[])
 {
+	QSvgWidget svg_widget;
+
 	qInfo() << "Test done.";
 }
--- repos/libports/src/test/qt6/qt_core_cmake/target.mk
+++ repos/libports/src/test/qt6/qt_core_cmake/target.mk
@@ -2,6 +2,6 @@ CMAKE_LISTS_DIR = $(PRG_DIR)
 
 CMAKE_TARGET_BINARIES = test-qt_core_cmake
 
-QT6_PORT_LIBS = libQt6Core
+QT6_PORT_LIBS = libQt6Core libQt6SvgWidgets libQt6Svg libQt6Widgets libQt6Gui
 
-LIBS = qt6_cmake libc libm qt6_component stdcxx
+LIBS = qt6_cmake libc libm qt6_component stdcxx mesa egl
