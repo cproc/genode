1a29059edece7c1769c1548bd3595f94e6caca9f
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-12 21:42:16 +0100

    libc_mmap.diff

--- repos/libports/src/lib/libc/internal/init.h
+++ repos/libports/src/lib/libc/internal/init.h
@@ -69,7 +69,9 @@ namespace Libc {
 	 * Virtual file system
 	 */
 	void init_vfs_plugin(Monitor &, Genode::Env::Local_rm &);
-	void init_file_operations(Cwd &, File_descriptor_allocator &, Config_accessor const &);
+	void init_file_operations(Cwd &, File_descriptor_allocator &, Config_accessor const &,
+	                          Ram_allocator &ram,
+	                          Genode::Env::Local_rm &rm);
 	void init_pread_pwrite(File_descriptor_allocator &);
 
 	/**
