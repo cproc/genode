b0696cac5e2b33c931f5ad182fa68160c9143fbb
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-25 10:09:19 +0100

    qt6_thermostat_example.diff

---
+++ repos/libports/src/app/qt6/examples/thermostat/target.mk
@@ -0,0 +1,18 @@
+CMAKE_LISTS_DIR = $(PRG_DIR)
+
+CMAKE_TARGET_BINARIES = ThermostatApp
+
+QT6_PORT_LIBS = libQt6Charts \
+                libQt6Core \
+                libQt6Gui \
+                libQt6Network \
+                libQt6OpenGL \
+                libQt6OpenGLWidgets \
+                libQt6Qml \
+                libQt6QmlModels \
+                libQt6Quick \
+                libQt6QuickControls2 \
+                libQt6Svg \
+                libQt6Widgets
+
+LIBS = qt6_cmake libc libm qt6_component stdcxx mesa egl
