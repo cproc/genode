e093590336f57e1552f2f55209e22c15da6e06a2
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-25 13:45:59 +0100

    qt6_qmlweather_example.diff

---
+++ repos/libports/src/app/qt6/examples/qmlweather/target.mk
@@ -0,0 +1,16 @@
+CMAKE_LISTS_DIR = $(PRG_DIR)
+
+CMAKE_TARGET_BINARIES = qmlweather
+
+QT6_PORT_LIBS = libQt6Charts \
+                libQt6Core \
+                libQt6Gui \
+                libQt6Network \
+                libQt6OpenGL \
+                libQt6OpenGLWidgets \
+                libQt6Qml \
+                libQt6QmlModels \
+                libQt6Quick \
+                libQt6Widgets
+
+LIBS = qt6_cmake libc libm qt6_component stdcxx mesa egl
