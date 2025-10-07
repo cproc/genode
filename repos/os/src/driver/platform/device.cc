3c395d2daa554baa7ba160b714eb66e0148e00c3
Christian Helmuth <christian.helmuth@genode-labs.com>
2025-10-07 16:11:11 +0200

    squash "platform: add explicit wc flag for I/O memory"
    forward wc attribute in devices ROM
    
    Issue #5703

--- repos/os/src/driver/platform/device.cc
+++ repos/os/src/driver/platform/device.cc
@@ -135,6 +135,7 @@ void Driver::Device::generate(Generator &g, bool info) const
 					return;
 				g.attribute("phys_addr", String<16>(Hex(io_mem.range.start)));
 				g.attribute("size",      String<16>(Hex(io_mem.range.size)));
+				g.attribute("wc",        io_mem.write_combined);
 			});
 		});
 		_irq_list.for_each([&] (Irq const &irq) {
