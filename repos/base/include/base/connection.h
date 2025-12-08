ef3b17017bf338af275374fbf337eb0410644699
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2025-12-08 14:21:51 +0100

    base: introduce common retry util in Connection
    Analogue to Connection::retry_with_upgrade this commit introduces
    a generic Connection::retry utility that works for calls returning
    an Attempt. The utility filters "OUT_OF_RAM" and "OUT_OF_CAPS"
    values out of the potential error return value, and upgrades the
    quota accordingly.
    
    Fix #5769

--- repos/base/include/base/connection.h
+++ repos/base/include/base/connection.h
@@ -84,6 +84,42 @@ class Genode::Connection_base : Noncopyable, Interface
 				[&] { upgrade_ram(ram.value); },
 				UPGRADE_ATTEMPTS);
 		}
+
+		/**
+		 * Extend session quota on demand while calling an RPC function,
+		 * which returns an Attempt<> value.
+		 *
+		 * \param ram   amount of RAM to upgrade as response to 'Out_of_ram'
+		 * \param caps  amount of caps to upgrade as response to 'Out_of_caps'
+		 *
+		 * \noapi
+		 */
+		auto retry(Ram_quota ram, Cap_quota caps, auto const &fn) -> decltype(fn())
+		{
+			while (true) {
+				auto ret = fn();
+				bool done = ret.template convert<bool>(
+					[&] (auto) { return true; },
+					[&] (auto error) {
+						switch (error) {
+						case decltype(error)::OUT_OF_CAPS:
+							{
+								upgrade_caps(caps.value);
+								return false;
+							}
+						case decltype(error)::OUT_OF_RAM:
+							{
+								upgrade_ram(ram.value);
+								return false;
+							}
+						default: break;
+						}
+						return true;
+					});
+				if (done)
+					return ret;
+			}
+		}
 };
 
 
