a5527c85cb8637991c83bb12fcd0a23ea3e371ed
Christian Prochaska <christian.prochaska@genode-labs.com>
2026-05-13 13:43:29 +0200

    TMP: non-blocking pipe test case

--- repos/libports/src/test/libc_pipe/main.cc
+++ repos/libports/src/test/libc_pipe/main.cc
@@ -13,77 +13,48 @@
 
 
 /* libc includes */
-#include <pthread.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 
-enum { BUF_SIZE = 16*1024 };
+enum { BUF_SIZE = 128*1024 };
 static char buf[BUF_SIZE];
 
 static int pipefd[2];
 
-static volatile bool reader_finished = false;
-
-void *read_pipe(void *arg)
-{
-	static char read_buf[BUF_SIZE];
-
-	ssize_t num_bytes_read = 0;
-
-	while (num_bytes_read < BUF_SIZE) {
-
-		ssize_t res = read(pipefd[0],
-		                   &read_buf[num_bytes_read],
-		                   BUF_SIZE - num_bytes_read);
-
-		if (res < 0) {
-			fprintf(stderr, "Error reading from pipe\n");
-			exit(1);
-		}
-
-		num_bytes_read += res;
-	}
-
-	if ((read_buf[0] != buf[0]) ||
-	    (read_buf[BUF_SIZE - 1] != buf[BUF_SIZE - 1])) {
-		fprintf(stderr, "Error: data mismatch\n");
-		exit(1);
-	}
-
-	reader_finished = true;
-
-	return 0;
-}
-
-
 int main(int argc, char *argv[])
 {
-	/* test values */
-	buf[0] = 1;
-	buf[BUF_SIZE - 1] = 255;
+	pipe2(pipefd, O_NONBLOCK);
 
-	int res = pipe(pipefd);
-	if (res != 0) {
-		fprintf(stderr, "Error creating pipe\n");
-		exit(1);
+	ssize_t bytes_written_total = 0;
+	ssize_t bytes_read_total = 0;
+
+	for (int i = 0; i < 2; i++) {
+
+		fprintf(stderr, "writing up to %d bytes\n", BUF_SIZE);
+
+		ssize_t bytes_written = write(pipefd[1], buf, BUF_SIZE);
+
+		if (bytes_written > 0)
+			bytes_written_total += bytes_written;
+
+		fprintf(stderr, "wrote %zd bytes (buffered: %zd)\n", bytes_written, bytes_written_total - bytes_read_total);
+
+		sleep(1);
+
+		fprintf(stderr, "reading up to %d bytes\n", BUF_SIZE);
+
+		ssize_t bytes_read = read(pipefd[0], buf, BUF_SIZE);
+
+		if (bytes_read > 0)
+			bytes_read_total += bytes_read;
+
+		fprintf(stderr, "read %zd bytes (buffered: %zd)\n", bytes_read, bytes_written_total - bytes_read_total);
+
+		sleep(1);
 	}
-
-	pthread_t tid;
-	pthread_create(&tid, 0, read_pipe, 0);
-
-	ssize_t bytes_written = write(pipefd[1], buf, BUF_SIZE);
-
-	if (bytes_written != BUF_SIZE) {
-		fprintf(stderr, "Error writing to pipe (bytes_written=%zd, BUF_SIZE=%zd)\n",
-		        bytes_written, (size_t)BUF_SIZE);
-		exit(1);
-	}
-
-	pthread_join(tid, NULL);
-
-	printf("--- test finished ---\n");
 
 	return 0;
 }
